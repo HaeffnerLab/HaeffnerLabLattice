930cbe454587c5f140e881da83ca5e7216fe7d22
Michael Ramm <micramm@gmail.com>
2011-07-27 14:21:49 -0700

    debugging multiplexer

--- lattice/serialdevices/multiplexer/Multiplexer.py
+++ lattice/serialdevices/multiplexer/Multiplexer.py
@@ -101,7 +101,6 @@ class Multiplexer( SerialDeviceServer ):
     @inlineCallbacks
     def initServer( self ):
         self.createChannelInfo()
-        print [(i.state,i.exp) for i in self.channelinfolist]
         if not self.regKey or not self.serNode: raise SerialDeviceError( 'Must define regKey and serNode attributes' )
         port = yield self.getPortFromReg( self.regKey )
         self.port = port
