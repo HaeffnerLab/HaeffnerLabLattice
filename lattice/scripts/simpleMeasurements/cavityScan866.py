b9bb1134da2573a96b9eb79f9c85116490ac5492
Michael Ramm <micramm@gmail.com>
2011-11-30 19:40:11 -0800

    day to day experiments

--- lattice/scripts/simpleMeasurements/cavityScan866.py
+++ lattice/scripts/simpleMeasurements/cavityScan866.py
@@ -3,5 +3,5 @@ import labrad
 from scriptLibrary.cavityScan import scanCavity  
 
 cxn = labrad.connect()
-scanCavity(cxn, ch = '866', resolution = 2, min = 450.0, max = 570.0, average = 3)
+scanCavity(cxn, ch = '866', resolution = 2, min = 450.0, max = 650.0, average = 3)
 print 'DONE'
