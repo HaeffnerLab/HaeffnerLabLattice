1083fb1dba423612b370be91a3d2682245e437e6
Michael Ramm <micramm@gmail.com>
2012-02-17 18:39:21 -0800

    pulse sequence

--- lattice/okfpgaservers/pulser_sequence.py
+++ lattice/okfpgaservers/pulser_sequence.py
@@ -8,9 +8,9 @@ MAX_SEQUENCE = 85 #seconds
 class hardwareConfiguration():
     
     channelDict = {
-                   '0':0,
-                   'testChannel':0,
-                   '1':1
+                   '1':1,
+                   'testChannel':1,
+                   '2':2
                    }
 
 class Sequence():
@@ -18,7 +18,6 @@ class Sequence():
     def __init__(self):
         self.channelDict = hardwareConfiguration.channelDict
         self.switchingTimes = {0:0} #dictionary in the form time:which channels to swtich
-        self.seqEnd = 0
     
     def addTTLPulse(self, channel, start, duration):
         """adding TTL pulse, times are in seconds"""
@@ -27,7 +26,6 @@ class Sequence():
         if not duration >= timeResolution: raise Exception ("Incorrect duration") 
         hardwareAddr = self.channelDict[channel]
         for t in [start, start+duration]: self._addNewSwitch(self.secToStep(t), hardwareAddr)
-        self.seqEnd = max(self.seqEnd, self.secToStep(start + duration))
 
     def secToStep(self, sec):
         '''converts seconds to time steps'''
@@ -37,25 +35,20 @@ class Sequence():
         '''converts the number to the hex representation for a total of 32 bits
         i.e: 3 -> 00000000...000100 ->  \x00\x00\x00\x03'''
         a,b = number // 65536, number % 65536
-        print str(numpy.uint16([a,b]).data)[0]
         return str(numpy.uint16([a,b]).data)
 
     def _addNewSwitch(self, time, addr):
         if self.switchingTimes.has_key(time):
-            self.switchingTimes[time] += 2**addr
+            self.switchingTimes[time] += 2**(addr-1)
         else:
-            self.switchingTimes[time] = 2**addr
-    
-    def _addLast(self):
-        """adds the terminating pulse to the sequence"""
-        self._addNewSwitch(self.seqEnd, 0)
-        
+            self.switchingTimes[time] = 2**(addr-1)
+           
     def progRepresentation(self):
         """Returns the representation of the sequence for programming the FPGA"""
-        self._addLast()
         rep = ''
         for key,channels in sorted(self.switchingTimes.iteritems()):
             rep = rep + self.numToHex(key) + self.numToHex(channels)
+        rep = rep + 2*self.numToHex(0) #adding termination
         return rep
     
     def humanRepresentation(self):
