cbc849d0ce0217d3e5a4603dee92f49fbbf0db17
Michael Ramm <micramm@gmail.com>
2012-02-23 01:12:43 -0800

    AC coming along

--- lattice/miscservers/TempTaker2.py
+++ lattice/miscservers/TempTaker2.py
@@ -25,15 +25,39 @@ import numpy
 #store PID, integrator in the registry
 class AlarmChecker():
 
-    timeToReset = 6*3600  # set time for next alarm to 6 hours
+    timeToReset = 2*3600  # set time for next alarm to 2 hours
     messageMax = 1 #maximum number of allowed emails per the number of callstoReset
     
-    def __init__(self, emailer):
+    def __init__(self, emailer, dict):
+        self.Table1 = dict['Table1']
+        self.Table3 = dict['Table3']
+        self.Table4 = dict['Table4']
+        self.ColdWater = dict['ColdWater']
+        self.HotWater = dict['HotWater']
         self.emailer = emailer
         self.messageSent = 0
+        self.alarmReset = LoopingCall(self.reset)
+        self.alarmReset.start(self.timeToReset, now = False)
+        
+    def reset(self):
+        self.messageSent = 0
     
-    def check(self, temperature):
-        pass
+    @inlineCallbacks
+    def check(self, temp):
+        message = None
+        if(abs(temp[self.Table1] - 22) > 2):
+            message = ('AC ALARM','The differential between Table1 temperature and setpoint exceeds norm')
+        if(abs(temp[self.Table3] - 22) > 2):
+            message = ('AC ALARM','The differential between Table3 temperature and setpoint exceeds norm')
+        if(abs(temp[self.Table4] - 22) > 2):
+            message = ('AC ALARM','The differential between Table4 temperature and setpoint exceeds norm')
+        #if(abs(temp[self.ColdWater] - 9) > 9):
+            #message = ('AC ALARM','Cold Water temperature is too far from norm')
+        if(abs(temp[self.HotWater] - 45) > 35):  # Hot water varies really a lot
+            message = ('AC ALARM','The HotWaterBigRoom temperature is too far from norm')
+        if message and (self.messageSent < self.messageMax): 
+            yield self.emailer.send(*message)
+            self.messageSent +=1
 
 class RunningAverage():
     """Allows for smoothing of input data by taking a running average"""
@@ -64,16 +88,23 @@ class AC_Server( LabradServer ):
         
     @inlineCallbacks
     def initServer( self ):
+        #setting up necessary objects
         self.daq = DataAcquisition()
         self.channels = self.daq.channels
+        self.channelDict = self.daq.channelDict
         self.averager = RunningAverage(self.channels, averageNumber = 12)
+        self.emailer = self.client.emailer
+        yield self.emailer.set_recipients(['micramm@gmail.com']) # set this later
+        self.alarmChecker = AlarmChecker(self.emailer, self.channelDict)
+        #stting up constants
         self.manualOverwrite = False
         self.manualPositions = None####
         self.PIDparams =[0,0,0]####get from registry
+        self.daqErrors = 0
+        #begin control
         self.inControl = LoopingCall(self.control)
         self.inControl.start(self.updateRate)
-        self.daqErrors = 0
-        yield None
+
 
     @setting(0, 'Manual Override', enable = 'b', valvePositions = '*v')
     def manualOverride(self, c, enable, valvePositions = None):
@@ -101,20 +132,180 @@ class AC_Server( LabradServer ):
         else:
             self.averager.add(temps)
             temps = self.averager.getAverage()
+            yield self.alarmChecker.check(temps)
             
-    
     @inlineCallbacks
     def checkMaxErrors(self):
         """checks how many errors have occured in sends out a notification email"""
         if self.daqErrors >  self.maxDaqErrors:
             print "TOO MANY DAQ ERRORS"
-            #yield self.emailer.send()...
+            yield self.emailer.send('AC ALARM: TOO MANY DAQ ERRORS', '')
             self.daqErrors = 0
+            self.inControl.stop()
     
+class ResponseCalculator():
+    def __init__(self):
+        self.lastErrSigArr = zeros(Ch) #initial vale of lastErrorSignal, used to disable Diff gain for the first time
+        self.loadExternalParams()    
+    def loadExternalParams(self):
+        if(os.path.isfile(integfile)):#if integ file exists (with information about last integration), open it in read/write mode and read in last integrator setting
+            self.INTEGFILE =  open(integfile,"r+");
+            self.integralerrorSigArr = array(pickle.load(self.INTEGFILE))
+        else: #if file does not exist, create it and specify initial integrator parameters.
+            self.INTEGFILE =  open(integfile,"w");
+            self.integralerrorSigArr = zeros(Ch)
+        if(os.path.isfile(pifile)): #if file exists, load the PI parameters
+            self.PIFILE = open(pifile,"r+")
+            self.P = array(pickle.load(self.PIFILE))
+            self.I = array(pickle.load(self.PIFILE))
+            self.D = array(pickle.load(self.PIFILE))
+            self.PIFILE.close()
+        else:
+            self.PIFILE = open(pifile,"w") #if file doesn't not exist, create it
+            #proportionality constant for PID in the format [#144 big room / #140 small room / #144B Laser Room / #144A office]
+            self.P = array([-15,-15,-15,-0])
+            self.I = array([-.1,-.1,-.1,-0])
+            self.D = array([-40,-40,-40,0])
+            pickle.dump(self.P.tolist(),self.PIFILE)
+            pickle.dump(self.I.tolist(),self.PIFILE)
+            pickle.dump(self.D.tolist(),self.PIFILE)
+            self.PIFILE.close()
+        self.PImodtime = os.path.getmtime(pifile) #time when pifile is last modified
+    def updateExternalPIDParams(self):
+        if(os.path.getmtime(pifile) != self.PImodtime): #if PI parmeters have been modified externally, update them
+            self.PIFILE = open(pifile, 'r')
+            self.P = array(pickle.load(self.PIFILE))
+            self.I = array(pickle.load(self.PIFILE))
+            self.D = array(pickle.load(self.PIFILE))
+            self.PIFILE.close()
+            self.PImodtime = os.path.getmtime(pifile)
+            print("new P,I,D parameters are")
+            print self.P
+            print self.I
+            print self.D
+            
+    def getResponse(self):
+        return [self.PIDresponseArr,self.valvesignalArr]
+    
+    def calculateResponse(self, curTempArr):
+        self.errorSigArr = self.finderrorSig(curTempArr)
+        self.integralerrorSigArr = self.calcintegrator(self.integralerrorSigArr, self.errorSigArr)
+        self.saveIntegralError(self.integralerrorSigArr)
+        self.PIDresponseArr = self.findPIDresponse(self.errorSigArr, self.integralerrorSigArr,self.lastErrSigArr) 
+        self.lastErrSigArr= self.errorSigArr
+        self.valvesignalArr = self.CalcValveSignal(self.PIDresponseArr, curTempArr)
+        
+    def saveIntegralError(self,integError):
+        #print integError
+        self.INTEGFILE.seek(0) #moves position to the beginning of the file
+        pickle.dump(integError, self.INTEGFILE)
+        self.INTEGFILE.truncate() 
+
+        
+    def finderrorSig(self, CurTemp): #takes array with current temperatures and finds the error signal array
+        error = CurTemp - SetPoint
+        return error
+        
+    def calcintegrator(self,oldArr, newArr):
+        TotalArr = oldArr + newArr
+        # Normalize maximum by the mean of the integration constants
+        minim = IntegrationMin/(-sum(self.I)/len(self.I))
+        maxim = IntegrationMax/(-sum(self.I)/len(self.I))
+        TotalArr=clip(TotalArr,minim,maxim)
+        return TotalArr
+            
+    def findPIDresponse(self,curErrArr, IntErrArr, lastErrArr):    #produces array containg signal to be sent to valves in format [Control1, Control2..] where each one is measured from -255 to 255 positive to hotter, negative for colder
+        P = self.P
+        I = self.I
+        D = self.D
+        propArr = zeros(ControlCh)
+        propArr[bigroomctrl] = PSup[bigroomctrl]*curErrArr[SupplyBigRoom-1] + PTab[bigroomctrl]*curErrArr[Table1-1] + PCoolingWater[bigroomctrl]*curErrArr[ColdWaterBigRoom]
+        propArr[smlroomctrl] = PSup[smlroomctrl]*curErrArr[SupplySmallRoom-1] + PTab[smlroomctrl]*curErrArr[Table3-1] + PCoolingWater[smlroomctrl]*curErrArr[ColdWaterSmallRoom]
+        propArr[laserroomctrl] = PSup[laserroomctrl]*curErrArr[SupplyLaserRoom-1] + PTab[laserroomctrl]*curErrArr[Table4-1] + PCoolingWater[laserroomctrl]*curErrArr[ColdWaterLaserRoom]
+        propArr[officectrl] = 0 #no control in office
+        propArr = propArr - clip(propArr, -PropActionThreshold,PropActionThreshold)
+    
+        proprespArr = (P * propArr) # when used with arrays, * is component by component multiplcation or dot product for 1D arrays
+    
+        integArr =  zeros(ControlCh)
+        integArr[bigroomctrl] = IntErrArr[Table1-1]
+        integArr[smlroomctrl] = IntErrArr[Table3-1]
+        integArr[laserroomctrl] = IntErrArr[Table4-1]
+        integArr[officectrl] = 0 #no control in office
+        integrespArr = (I * integArr) # when used with arrays, * is component by component multiplcation or dot product for 1D arrays
+        #print integArr
+        
+        if((lastErrArr == zeros(Ch)).any()): #when the lastErrArr is the zero array, then don't do any diff because it's the first run
+            diffrespArr = zeros(ControlCh)
+        else:
+            diffArr =  zeros(ControlCh)
+            DiffErrArr = curErrArr - lastErrArr
+            diffArr[bigroomctrl] = DiffErrArr[SupplyBigRoom-1] + ColdWaterDiffGain[bigroomctrl] * DiffErrArr[ColdWaterBigRoom-1] / D[bigroomctrl]
+            diffArr[smlroomctrl] = DiffErrArr[SupplySmallRoom-1] + ColdWaterDiffGain[smlroomctrl] * DiffErrArr[ColdWaterSmallRoom-1] / D[smlroomctrl]
+            diffArr[laserroomctrl] = DiffErrArr[SupplyLaserRoom-1] + ColdWaterDiffGain[laserroomctrl] * DiffErrArr[ColdWaterLaserRoom-1] / D[laserroomctrl]
+            diffArr[officectrl] = 0 # no control in office
+            diffArr = diffArr - clip(diffArr, -DiffActionThreshold,DiffActionThreshold)
+
+            diffrespArr = (D * diffArr)
+            diffrespArr = clip(diffrespArr, -DiffMax, DiffMax)
+        
+        responseArr = proprespArr + integrespArr + diffrespArr    
+        return responseArr
+            
+    def CalcValveSignal(self,responseArr,curTempArr):#hard codes which control channel correspond to which output number
+        valvesignalArr = zeros(ControlledValves)
+    
+        ColdWater = array([curTempArr[ColdWaterBigRoom-1], curTempArr[ColdWaterSmallRoom-1], curTempArr[ColdWaterLaserRoom-1],0 ])
+        ColdWater = clip(ColdWater,0,20)
+#        ColdWater = array([10,10,10,0]);   # set cold water temp to 10 degrees because the sensor is not working atm
+
+        HotWater = array([curTempArr[HotWaterBigRoom-1], curTempArr[HotWaterSmallRoom-1], curTempArr[HotWaterLaserRoom-1], 0])
+        SetPointAux = array([SetPoint[Table1-1], SetPoint[Table3-1], SetPoint[Table4-1], 0])  
+
+        CoolingPower = clip(SetPointAux - ColdWater - ColdWaterTempCorrection,1,100) # estimate cooling power for valve settings, always assume some cooling power
+        HeatingPower =  clip(HotWater - SetPointAux,20,200) # minum heating power corresponds to 20 degrees temp-difference
+
+        ColdValveSignal = - responseArr/CoolingPower*ColdValveGain + Coldoffset + ColdWaterValveGain * (ColdWater-ColdWaterTempBase)
+        HotValveSignal = Hotoffset + responseArr/HeatingPower*HotValveGain
+        
+                valvesignalArr[0] = ColdValveSignal[smlroomctrl]
+                valvesignalArr[1] = HotValveSignal[smlroomctrl]
+                valvesignalArr[2] = ColdValveSignal[bigroomctrl]
+                valvesignalArr[3] = HotValveSignal[bigroomctrl]
+                valvesignalArr[4] = ColdValveSignal[laserroomctrl]
+                valvesignalArr[5] = HotValveSignal[laserroomctrl]
+                valvesignalArr[6] = 0
+                valvesignalArr[7] = 0
+
+#                valvesignalArr[0] = clip(ColdValveSignal[smlroomctrl],ValveMin[0],ValveMax)
+#                valvesignalArr[1] = clip(HotValveSignal[smlroomctrl],ValveMin[1],ValveMax)
+#                valvesignalArr[2] = clip(ColdValveSignal[bigroomctrl],ValveMin[2],ValveMax)
+#                valvesignalArr[3] = clip(HotValveSignal[bigroomctrl],ValveMin[3],ValveMax)
+#                valvesignalArr[4] = clip(ColdValveSignal[laserroomctrl],ValveMin[4],ValveMax)
+#                valvesignalArr[5] = clip(HotValveSignal[laserroomctrl],ValveMin[5],ValveMax)
+#                valvesignalArr[6] = 0
+#                valvesignalArr[7] = 0
+    
+        valvesignal = valvesignalArr.tolist()
+
+        return valvesignalArr
+
 class DataAcquisition():
     channels = 16
     hardwareGain = numpy.array([15,15,15,15,15,15,15,15,15,15,15,15,5,15,5,15]) #channels 13 and 15 (or 12,14 counting from 0) have less gain for expanded range
     V0 = 6.95 #volts on the voltage reference
+    #dictionary of locations and corresponding array elements i.e table3 corresponds to hardwareGain[0]
+    channelDict = {'Table1':7,
+                   'Table2':4,
+                   'Table3':0,
+                   'Table4':5,
+                   'Table5':8,
+                   'SupplyBigRoom':13,
+                   'SupplyLaserRoom':15,
+                   'SupplySmallRoom':10,
+                   'ColdWater':14,
+                   'HotWater':12
+                   }
     
     def __init__(self, serial):
         self.serial = serial
