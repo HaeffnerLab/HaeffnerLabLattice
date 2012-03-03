57005a6d0e2eba93a5dc5d98c1a8c5235bef45dd
Michael Ramm <micramm@gmail.com>
2012-03-02 18:56:00 -0800

    plot melted fix

--- lattice/scripts/dataAnalysis/latentHeat/plot_melted.py
+++ lattice/scripts/dataAnalysis/latentHeat/plot_melted.py
@@ -1,14 +1,16 @@
+import numpy
+
 datasetName = '2012Feb27_1808_29'
-melted = set([2,3,7,8,9,15,16,20,21,24,25,26,27,28,29,30,31,32,36,37,38,40,
+meltedCamera = [2,3,7,8,9,15,16,20,21,24,25,26,27,28,29,30,31,32,36,37,38,40,
           41,45,46,57,58,60,61,62,63,66,69,71,72,73,74,75,77,78,81,82,83,84,
-          87,90,93,94,95,98,100])
+          87,90,93,94,95,98,100]
+melted = set(101 - numpy.array(meltedCamera)) #order is reversed for saving on camera
 
 DIRECTORY = ['','Experiments','LatentHeat',datasetName,'timetags']
 
 notMelted = set(range(1,100)) - melted
 
 import labrad
-import numpy
 cxn = labrad.connect()
 dv = cxn.data_vault
 dv.cd(DIRECTORY)
@@ -26,14 +28,15 @@ notmeltedFluor = numpy.zeros(binNumber)
 
 for dataset in notMelted:
     print dataset
-    dv.open(dataset)
+    dv.open(int(dataset))
     timetags = dv.get().asarray[:,0]
     newbinned = numpy.histogram(timetags, binArray )[0]
     notmeltedFluor = notmeltedFluor + newbinned
 
 for dataset in melted:
     print dataset
-    dv.open(dataset)
+    print type(dataset)
+    dv.open(int(dataset))
     timetags = dv.get().asarray[:,0]
     newbinned = numpy.histogram(timetags, binArray )[0]
     meltedFluor = meltedFluor + newbinned
