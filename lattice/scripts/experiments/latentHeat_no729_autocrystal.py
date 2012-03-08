c91603eb43fcd458d7f0c2d2a5748f898215ef9e
Michael Ramm <micramm@gmail.com>
2012-03-07 20:16:20 -0800

    latent heat new

---
+++ lattice/scripts/experiments/latentHeat_no729_autocrystal.py
@@ -0,0 +1,145 @@
+import sys; sys.path.append('C:\\Users\\lattice\\Desktop\\LabRAD\\lattice\\scripts')
+import labrad
+import numpy
+import time
+import os
+from scriptLibrary.parameter import Parameters
+from scriptLibrary import paulsbox 
+from scriptLibrary import dvParameters 
+
+''''
+This experiment involves studying the sharpness of crystal to cloud phase transition. 
+After all cooling lights are switched off, the crystal is heated with far blue light for a variable time. Readout is meant to be done with a near resonant light.
+Typically vary:
+    axial_heat
+    heating delay
+Features:
+    independent control of hearing duration and waiting time afterwards
+    866 is switched off together with 397 to prevent cooling when it's not intended
+    automatic crystallization routine to reduce necessary time to crystallize the ions
+    frequency switching during the sequence with the RS list mode
+'''
+
+#Global parameters
+iterations = 50
+experimentName = 'LatentHeat_no729_autocrystal'
+#heating double pass frequency
+axfreq = 250.0 #MHz
+coolingfreq = 90.0 #MHz, frequency for the 110DP
+readoutfreq = 110.0 #Mhz, frequency for the 110DP
+auto_crystal = True
+#paul's box parameters
+pboxsequence = 'LatentHeat_no729_autocrystal.py' #all Paul's box parameters are in microseconds
+initial_cooling = 50.0*10**3
+heat_delay = 10.*10**3
+axial_heat = 25.0*10**3
+readout_delay = 25.*10**3
+readout_time = 50.*10**3
+
+#Time Resolved Recording
+recordTime = min(.55, 0.1 +  (initial_cooling + heat_delay + axial_heat + readout_delay + readout_time ) / 10.0**6)   
+
+globalDict = {
+              'iterations':iterations,
+              'experimentName':experimentName,
+              'axfreq':axfreq,
+              'recordTime':recordTime, 
+              'coolingfreq':coolingfreq,
+              'readoutfreq':readoutfreq
+              }
+
+pboxDict = {
+        'sequence':pboxsequence,
+        'initial_cooling':initial_cooling,
+        'heat_delay':heat_delay,
+        'axial_heat':axial_heat,
+        'readout_delay':readout_delay,
+        'readout_time':readout_time,
+        }
+
+#Binning on the fly
+binTime =250.0*10**-6
+binNumber = int(recordTime / binTime)
+binArray = binTime * numpy.arange(binNumber + 1)
+#parameters
+parameters = Parameters(globalDict)
+parameters.addDict(pboxDict)
+#connect and define servers we'll be using
+cxn = labrad.connect()
+cxnlab = labrad.connect('192.168.169.49') #connection to labwide network
+dv = cxn.data_vault
+dpass = cxn.double_pass
+trigger = cxn.trigger
+pbox = cxn.paul_box
+trfpga = cxn.timeresolvedfpga
+
+dirappend = time.strftime("%Y%b%d_%H%M_%S",time.localtime())
+
+def initialize():
+    trfpga.set_time_length(recordTime)
+    paulsbox.program(pbox, pboxDict)
+    trigger.switch_auto('axial',  False) #axial needs to be inverted, so that high TTL corresponds to light ON
+    trigger.switch_auto('110DP',  True) #high TTL corresponds to light OFF
+    trigger.switch_auto('866DP',  True) #high TTL corresponds to light OFF
+    trigger.switch_manual('crystallization',  False) #high TTL corresponds to light OFF
+    #make sure r&s synthesizers are on, and are of correct frequency
+    #heating
+    dpass.select('axial')
+    dpass.frequency(axfreq)
+    dpass.output(True)
+    #readout / cooling
+    dpass.select('110DP')
+    dpass.output(True)
+    #repump
+    dpass.select('repump')
+    dpass.output(True)
+    ####ADD LIST MODE HERE and RUN IT
+    globalDict['resolution']=trfpga.get_resolution()
+
+def sequence():
+    binnedFlour = numpy.zeros(binNumber)
+    for iteration in range(iterations):
+        print 'recording trace {0} out of {1}'.format(iteration+1, iterations)
+        trfpga.perform_time_resolved_measurement()
+        trigger.trigger('PaulBox')
+        timetags = trfpga.get_result_of_measurement().asarray
+        #saving timetags
+        dv.cd(['','Experiments', experimentName, dirappend, 'timetags'],True )
+        dv.new('timetags iter{0}'.format(iteration),[('Time', 'sec')],[('PMT counts','Arb','Arb')] )
+        dv.add_parameter('iteration',iteration)
+        ones = numpy.ones_like(timetags)
+        dv.add(numpy.vstack((timetags,ones)).transpose())
+        #add to binning of the entire sequence
+        newbinned = numpy.histogram(timetags, binArray )[0]
+        binnedFlour = binnedFlour + newbinned
+        print 'now waiting to complete recooling'
+        trigger.wait_for_pbox_completion()
+        trigger.wait_for_pbox_completion() #have to call twice until bug is fixed
+        if auto_crystal: auto_crystalize()
+    print 'getting result and adding to data vault'
+    dv.cd(['','Experiments', experimentName, dirappend] )
+    dv.new('binnedFlourescence',[('Time', 'sec')], [('PMT counts','Arb','Arb')] )
+    data = numpy.vstack((binArray[0:-1], binnedFlour)).transpose()
+    dv.add(data)
+    dv.add_parameter('plotLive',True)
+    print 'gathering parameters and adding them to data vault'
+    measureList = ['trapdrive','endcaps','compensation','dcoffsetonrf','cavity397','cavity866','multiplexer397','multiplexer866','axialDP']
+    measuredDict = dvParameters.measureParameters(cxn, cxnlab, measureList)
+    dvParameters.saveParameters(dv, measuredDict)
+    dvParameters.saveParameters(dv, globalDict)
+    dvParameters.saveParameters(dv, pboxDict)
+
+def finalize():
+    for name in ['axial', '110DP']:
+        trigger.switch_manual(name)
+
+def auto_crystalize():
+    print 'auto xtal'
+            
+print 'initializing measurement'
+initialize()
+print 'performing sequence'
+sequence()
+print 'finalizing'
+finalize()
+print 'DONE'
