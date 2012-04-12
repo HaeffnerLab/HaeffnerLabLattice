eda302092d1eef7f945590285ad1fd95cd72aaea
Michael Ramm <micramm@gmail.com>
2012-04-11 22:01:21 -0700

    ui cleanup

--- lattice/clients/qtui/testuiload.py
+++ lattice/clients/qtui/testuiload.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import sys
 from PyQt4 import QtGui
-from PyQt4 import QtCore,uic
+from PyQt4 import uic
 
 class DC_CONTROL(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
-        uic.loadUi('dconrf.ui',self)
+        uic.loadUi('timeresolvedfrontend.ui',self)
 
 app = QtGui.QApplication(sys.argv)
 icon = DC_CONTROL()
