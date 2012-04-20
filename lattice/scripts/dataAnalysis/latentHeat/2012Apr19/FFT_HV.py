1ab3fce34e547f772937a4c9932036b7f24c531c
Ishan Talukdar <ishan15@gmail.com>
2012-04-20 15:23:55 -0700

    2012Apr19_data

---
+++ lattice/scripts/dataAnalysis/latentHeat/2012Apr19/FFT_HV.py
@@ -0,0 +1,26 @@
+import numpy
+import labrad
+cxn = labrad.connect()
+dv = cxn.data_vault
+import matplotlib
+matplotlib.use('Qt4Agg')
+from matplotlib import pyplot
+
+dv.cd(['','QuickMeasurements','FFT'])
+dv.open(1460)
+data=dv.get()
+data = numpy.transpose(data)  # transposed
+x = data[0]; 
+y = data[1];
+x2 = numpy.linspace(0,8000,100)
+y2 = -5.1e-4*x2 + 3.8
+
+figure = pyplot.figure()
+figure.clf()
+pyplot.plot(x,y*1e5,'o')
+pyplot.hold('True')
+pyplot.plot(x2,y2)
+pyplot.xlabel('Compensation (V)')
+pyplot.ylabel('FFT signal (arb)')
+
+# need ~ 7.5 kV
---
+++ lattice/scripts/dataAnalysis/latentHeat/2012Apr19/plot_readout_Histogram.py
@@ -0,0 +1,50 @@
+import numpy
+import labrad
+cxn = labrad.connect()
+dv = cxn.data_vault
+import matplotlib
+matplotlib.use('Qt4Agg')
+from matplotlib import pyplot
+
+totalTraces = 50
+datasets = ['2012Apr19_2046_42']
+
+
+refSigs = []
+detectedCounts = [] #list of counts detected during readout
+
+figure = pyplot.figure()
+figure.clf()
+pyplot.suptitle('Histogram of readouts')
+
+for datasetName in datasets:
+    print 'Getting heating counts...'
+    dv.cd(['','Experiments','LatentHeat_no729_autocrystal',datasetName])
+    dv.open(1)    
+    initial_cooling = dv.get_parameter('initial_cooling')
+    heat_delay = dv.get_parameter('heat_delay')
+    axial_heat = dv.get_parameter('axial_heat')
+    readout_delay = dv.get_parameter('readout_delay')
+    readout_time = dv.get_parameter('readout_time')
+    xtal_record = dv.get_parameter('xtal_record')
+#    
+    # readout range
+    heatStart = (initial_cooling + heat_delay ) # / 10.0**6 #in seconds
+    heatEnd = (initial_cooling + heat_delay +axial_heat ) 
+    startReadout =  (initial_cooling + heat_delay + axial_heat + readout_delay ) 
+    stopReadout = startReadout + readout_time 
+    print datasetName, heatStart, heatEnd, startReadout, stopReadout
+    print 'Heating time :', heatEnd - heatStart
+    print 'Delay time :', readout_delay
+    dv.cd(['','Experiments','LatentHeat_no729_autocrystal',datasetName,'timetags'])
+        
+    for dataset in range(1,totalTraces+1):
+        dv.open(int(dataset))
+        timetags = dv.get().asarray[:,0]
+        countsReadout = numpy.count_nonzero((startReadout <= timetags) * (timetags <= stopReadout))
+        #normCounts = (float(refReadout) / float(max(refSigs))) * (countsReadout)  
+        detectedCounts.append(countsReadout)
+    print countsReadout
+print 'Done'
+pyplot.hist(detectedCounts, 60)
+pyplot.show()
