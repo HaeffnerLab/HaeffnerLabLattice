34eee57b5d4ffb0971594635508c34bc62e95852
Michael Ramm <micramm@gmail.com>
2012-04-26 01:08:15 -0700

    data processing on the fly

--- lattice/scripts/experiments/collectionEfficiency/collectionEfficiency.py
+++ lattice/scripts/experiments/collectionEfficiency/collectionEfficiency.py
@@ -6,8 +6,9 @@ import numpy
 import time
 from scriptLibrary import dvParameters 
 from PulseSequences.collectionEfficiency import collectionEfficiency
+import dataProcessor
 
-iterations = 100;
+repeatitions = 100;
 params = {
           'dopplerCooling':100e-3,
           'iterDelay':1e-3,
@@ -24,6 +25,7 @@ dirappend = time.strftime("%Y%b%d_%H%M_%S",time.localtime())
 cxn = labrad.connect()
 dv = cxn.data_vault
 pulser = cxn.pulser
+do = dataProcessor.dataProcessor(params)
 #pulse sequence
 pulser.new_sequence()
 seq = collectionEfficiency(pulser)
@@ -32,27 +34,11 @@ seq.defineSequence()
 pulser.program_sequence()
 #light logic
 pulser.switch_auto('866DP', True) #high TTL means light ON
-pulser.switch_manual('crystallization', False)
 pulser.switch_auto('110DP', True)
-#data processing on the fly
-repumpD = params['repumpD']
-repumpDelay = params['repumpDelay']
-exciteP = params['exciteP']
-finalDelay = params['finalDelay']
-dopplerCooling = params['dopplerCooling']
-cycleTime = repumpD + repumpDelay + exciteP + finalDelay
-binTime = 40.0*10**-9 #fpga resolution
-
-binNumber = int(cycleTime / binTime)
-bins = binTime * numpy.arange(binNumber + 1)
-binned = numpy.zeros(binNumber)
-#data processing on the fly
-dopplerBinTime = 1.0*10**-3
-dopplerBinNumber = int(dopplerCooling / dopplerBinTime)
-dopplerBins = dopplerBinTime * numpy.arange(dopplerBinNumber + 1)
-dopplerBinned = numpy.zeros(dopplerBinNumber)
-
-for i in range(iterations):
+pulser.switch_manual('crystallization', False)
+#create directory
+dv.cd(['','Experiments', experimentName, dirappend, 'timetags'],True )
+for i in range(repeatitions):
     pulser.reset_timetags()
     pulser.start_single()
     pulser.wait_sequence_done()
@@ -60,14 +46,19 @@ for i in range(iterations):
     timetags = pulser.get_timetags().asarray
     print 'got {0} timetags on iteration {1}'.format(timetags.size, i + 1)
     #saving timetags
-    dv.cd(['','Experiments', experimentName, dirappend, 'timetags'],True )
     dv.new('timetags iter{0}'.format(i),[('Time', 'sec')],[('PMT counts','Arb','Arb')] )
     dv.add_parameter('iteration',i)
     ones = numpy.ones_like(timetags)
     dv.add(numpy.vstack((timetags,ones)).transpose())
     dvParameters.saveParameters(dv, params)
+    #data processing on the fuly
+    dp.addTimetags(timetags)
 
 #revert the logic
 pulser.switch_auto('866DP', False) #high TTL means light Off
 pulser.switch_manual('110DP', True)
-print 'DONE {}'.format(dirappend)
+#complete
+print 'SAVED {}'.format(dirappend)
+dp.normalize(repeations)
+dp.makePlot()
+print 'DONE'
