7d3df702713cf876b376f9d2e8f33b8eaf3b4f53
Michael Ramm <micramm@gmail.com>
2012-04-30 17:09:55 -0700

    collection efficiency analysis

---
+++ lattice/scripts/dataAnalysis/collectionEfficiency/2012April27/plottogether.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Qt4Agg')
+from matplotlib import pyplot
+
+datasets = [
+            ('397: -12dBm 866: 0 deg', '2012Apr27_1817_45binning.npz'),
+            ('397: -12dBm 866: +22.5deg','2012Apr27_1849_26binning.npz'),
+            ('397: -12dBm 866: +45.0deg','2012Apr27_1918_55binning.npz'),
+            ('397: -4.0dBm 866: 0 deg','2012Apr27_1949_31binning.npz')
+            ]
+
+pyplot.figure()
+for data in datasets:
+    label,name = data
+    f = np.load(name)
+    bins = f['bins']
+    binned = f['binned']
+    pyplot.plot(bins[0:-1],binned, label = label)
+ax = pyplot.gca()
+ax.ticklabel_format(style = 'sci', scilimits = (0,0), axis = 'x')
+pyplot.xlabel('Sec')
+pyplot.ylabel('Counts/Sec')
+pyplot.legend()
+pyplot.show()
