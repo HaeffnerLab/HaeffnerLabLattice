ff738f71b36eed697620df63fd45327aa2030177
Michael Ramm <micramm@gmail.com>
2012-06-15 16:41:26 -0700

    dds bug fixes

--- lattice/scripts/PulseSequences/sampleDDS.py
+++ lattice/scripts/PulseSequences/sampleDDS.py
@@ -12,41 +12,32 @@ class sampleDDS(Sequence):
     def defineSequence(self):
         pulser = self.pulser
         p = self.parameters
-        pulser.add_dds_pulses('866DP', [(100e-9, 65.0 , -33.0)])
-        pulser.add_dds_pulses('866DP', [(.1 + 100e-9, 70.0 , -33.0)])
-        pulser.add_dds_pulses('866DP', [(.2 + 100e-9, 75.0 , -33.0)])
-        pulser.add_dds_pulses('866DP', [(.3 + 100e-9, 80.0 , -33.0)])
-        pulser.add_dds_pulses('axial', [(100e-9, 95.0 , -3.0)])
-        pulser.add_dds_pulses('axial', [(.1 + 100e-9, 90.0 , -3.0)])
-        pulser.add_dds_pulses('axial', [(.2 + 100e-9, 85.0 , -3.0)])
-        pulser.add_dds_pulses('854DP', [(100e-9, 95.0 + 5, -13.0)])
-        pulser.add_dds_pulses('854DP', [(.1 + 100e-9, 90.0 + 5 , -13.0)])
-        pulser.add_dds_pulses('854DP', [(.2 + 100e-9, 85.0 + 5 , -13.0)])
-        pulser.add_dds_pulses('110DP', [(100e-9, 60.0 , -23.0)])
-        pulser.add_dds_pulses('110DP', [(.1 + 100e-9, 65.0 , -23.0)])
-        pulser.add_dds_pulses('110DP', [(.2 + 100e-9, 70.0 , -23.0)])
-        pulser.add_dds_pulses('110DP', [(.3 + 100e-9, 75.0 , -23.0)])
+        pwrs = numpy.linspace(-13,-3,101)
+        print pwrs
+        for iter,p in enumerate(pwrs):
+            t = 100e-9 + iter * .05
+            f = 219.6 + iter * .04
+            pulser.add_dds_pulses('axial', [(t , 220.0 , p)])
         
         
-        pulser.extend_sequence_length(0.5)
-            
 if __name__ == '__main__':
-    import labrad
-    cxn = labrad.connect()
-    pulser = cxn.pulser
-    seq = sampleDDS(pulser)
-    pulser.new_sequence()
-    seq.defineSequence()
-    pulser.program_sequence()
-#    pulser.start_single()
-    pulser.start_number(100)
-    #time.sleep(2)
-    pulser.wait_sequence_done(1000.0)
-    #print 'completed', pulser.repeatitions_completed()
-    pulser.stop_sequence()
-    #print 'completed', pulser.repeatitions_completed()
-#    hr = pulser.human_readable().asarray
-#    channels = pulser.get_channels().asarray
-#    sp = SequencePlotter(hr, channels)
-#    sp.makePlot()
-    print 'done'
+    for i in range(50):
+        import labrad
+        cxn = labrad.connect()
+        pulser = cxn.pulser
+        seq = sampleDDS(pulser)
+        pulser.new_sequence()
+        seq.defineSequence()
+        pulser.program_sequence()
+        pulser.start_single()
+    #    pulser.start_number(100)
+        #time.sleep(2)
+        pulser.wait_sequence_done(1000.0)
+        #print 'completed', pulser.repeatitions_completed()
+        pulser.stop_sequence()
+        #print 'completed', pulser.repeatitions_completed()
+    #    hr = pulser.human_readable().asarray
+    #    channels = pulser.get_channels().asarray
+    #    sp = SequencePlotter(hr, channels)
+    #    sp.makePlot()
+        print 'done'
