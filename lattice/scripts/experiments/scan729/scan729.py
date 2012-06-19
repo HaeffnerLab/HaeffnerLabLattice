6355f554ffd15df03722be96a91e870b3975b1d2
Michael Ramm <micramm@gmail.com>
2012-06-19 16:19:27 -0700

    729 plotting on the fly, histogram display

--- lattice/scripts/experiments/scan729/scan729.py
+++ lattice/scripts/experiments/scan729/scan729.py
@@ -6,7 +6,7 @@ import numpy
 import time
 from scriptLibrary import dvParameters 
 from PulseSequences.scan729 import scan729 as sequence
-from dataProcessor import data_process
+from dataProcessor import freqscan
 
 class Bunch:
     def __init__(self, **kwds):
@@ -28,7 +28,7 @@ class scan729():
     '''
     experimentName = 'scan729'
     
-    def __init__(self, seqParams, exprtParams):
+    def __init__(self, seqParams, exprtParams, analysisParams):
        #connect and define servers we'll be using
         self.cxn = labrad.connect()
         self.cxnlab = labrad.connect('192.168.169.49') #connection to labwide network
@@ -39,20 +39,24 @@ class scan729():
         self.pulser = self.cxn.pulser
         self.seqP = Bunch(**seqParams)
         self.expP = Bunch(**exprtParams)
+        self.anaP = Bunch(**analysisParams)
         
     def initialize(self):
         #directory name and initial variables
         self.dirappend = time.strftime("%Y%b%d_%H%M_%S",time.localtime())
+        self.directory = ['','Experiments', self.experimentName, self.dirappend]
         #saving
-        self.dv.cd(['','Experiments', self.experimentName, self.dirappend, 'timetags'],True )
+        self.dv.cd(self.directory ,True )
         self.dv.new('timetags',[('Freq', 'MHz')],[('Time','Sec','Sec')] )
         self.programPulser()
         self.setupLogic()
+        self.setupAnalysis()
         
     def setupLogic(self):
         self.pulser.switch_auto('axial',  True) #axial needs to be inverted, so that high TTL corresponds to light ON
         self.pulser.switch_auto('110DP',  False) #high TTL corresponds to light OFF
         self.pulser.switch_auto('866DP', False) #high TTL corresponds to light OFF
+        self.pulser.switch_auto('729DP', True)
         self.pulser.switch_manual('crystallization',  False)
     
     def programPulser(self):
@@ -64,6 +68,9 @@ class scan729():
         self.seqP['recordTime'] = seq.recordTime
         self.seqP['startReadout'] = seq.startReadout
         self.seqP['stopReadout'] = seq.stopReadout
+    
+    def setupAnalysis(self):
+        self.dp = freqscan(self.seqP.startReadout, self.seqP.stopReadout, self.dv, self.directory, threshold = self.anaP.threshold)
     
     def run(self):
         sP = self.seqP
@@ -79,16 +86,17 @@ class scan729():
         frequencies = numpy.arange(xP.freqMin, xP.freqMax + xP.freqStep, xP.freqStep)
         frequencies = frequencies[((frequencies >= xP.freqMin) * (frequencies <= xP.freqMax))] #prevents issues when step size > (max - min)
         for freq in frequencies:
-            print 'setting frequency {} MHz'.format(freq) 
             self.synth.frequency(freq)
             self.pulser.reset_timetags()
             self.pulser.start_number(xP.iterations)
             self.pulser.wait_sequence_done()
             self.pulser.stop_sequence()
             timetags = self.pulser.get_timetags().asarray
+            print 'frequency {} : got {} timetags'.format(freq, timetags.size)
             #saving timetags
             frqs = numpy.ones_like(timetags) * freq
             self.dv.add(numpy.vstack((frqs,timetags)).transpose())
+            self.dp.addFrequency(freq, timetags, addToPlot = True)
     
     def finalize(self):
         #go back to initiali frequency
@@ -102,6 +110,8 @@ class scan729():
         dvParameters.saveParameters(self.dv, measuredDict)
         dvParameters.saveParameters(self.dv, self.seqP.toDict())
         dvParameters.saveParameters(self.dv, self.expP.toDict())
+        #show histogram
+        self.dp.makeHistPlot()
         
     def __del__(self):
         self.cxn.disconnect()
@@ -109,24 +119,26 @@ class scan729():
 if __name__ == '__main__':
   
     params = {
+        'freq397':110.0,
+        'coolingPower397':-14.0,
+        'readoutPower397':-11.0,
         'backgroundMeasure':1*10**-3,
         'initial_cooling':5*10**-3,
         'optical_pumping':1*10**-3,
-        'rabitime':5*10**-3,
+        'rabitime':.5*10**-3,
         'readout_time':10*10**-3,
         'repump854':5*10**-3,
         'repumpPower':-3.0
           }
     exprtParams = {
-                   'freqMin':210.0,
-                   'freqMax':230.0,
-                   'freqStep':40.0,
-                   'iterations':20,
-                   'binTime':250.0*10**-6
-                   }
-    exprt = scan729(params,exprtParams)
+        'freqMin':219.0,
+        'freqMax':222.0,
+        'freqStep':0.1,
+        'iterations':100,
+        }
+    
+    analysis = {
+        'threshold':5,
+        }
+    exprt = scan729(params,exprtParams, analysis)
     exprt.run()
-    dp =  data_process(exprt.cxn, exprt.dirappend, ['','Experiments', exprt.experimentName, exprt.dirappend,'timetags'], ['freqscan'])
-    #dp.addParameter('threshold', 13000)
-    dp.loadDataVault()
-    dp.processAll()
