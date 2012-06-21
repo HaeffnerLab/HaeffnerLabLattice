d161bc941706a51d08fa459caf3e594dd51078b4
Firstname <haeffnerlab@gmail.com>
2012-06-20 18:59:33 -0700

    Save scans to data vault. Axis labels still broken (must be fixed because one can only zoom in once)

--- lattice/cdllservers/Andor/AndorVideo.py
+++ lattice/cdllservers/Andor/AndorVideo.py
@@ -145,19 +145,25 @@
 import sys
 from PyQt4 import QtGui, QtCore
 from matplotlib.figure import Figure
+from matplotlib import cm
 import time
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet.threads import deferToThread
+from datetime import datetime
 
 
 
-import matplotlib.pyplot as plt 
+
+from matplotlib.widgets import RectangleSelector 
 import numpy as np
 
-class CPUMonitor(FigureCanvas):
+EMGAIN = 255
+EXPOSURE = .3 #sec
+
+class Canvas(FigureCanvas):
     """Matplotlib Figure widget to display CPU utilization"""
     def __init__(self, parent):
 
@@ -165,6 +171,9 @@ class CPUMonitor(FigureCanvas):
         self.fig = Figure()
         self.ax = self.fig.add_subplot(111)
         FigureCanvas.__init__(self, self.fig)
+        #self.cidPress = self.mpl_connect('button_press_event', self.onClick)
+        #self.cidRelease = self.mpl_connect('button_release_event', self.onRelease)
+
 
         #self.X = np.random.rand(5,5)
         #rows,cols,self.slices = self.X.shape
@@ -183,49 +192,77 @@ class CPUMonitor(FigureCanvas):
         self.fig.canvas.draw()
 
         self.cnt = 0
+        
+        self.setupSelector()
         # call the update method (to speed-up visualization)
         #self.timerEvent(None)
         # start timer, trigger event every 1000 millisecs (=1sec)
         #self.timer = self.startTimer(500)
 
-    def startYourEngines(self):
-        self.timer = self.startTimer(50)
+#    def startYourEngines(self):
+#        self.timer = self.startTimer(50)
     
-    def timerEvent(self, evt):
-#        if (self.cnt == 12):
-#            self.im = self.ax.matshow(self.data)
-#            
-#        
-##        if (self.cnt == 0):
-##            self.cnt = 1
-##            self.im.set_data(self.X)
-##        elif (self.cnt == 1):
-##            self.im.set_data(self.Y)
-##            self.cnt = 0    
-#        elif (self.cnt > 12):    
-#            self.im.set_data(self.data)            
-#            #print self.data[0, 4:10]
-#            
+#    def timerEvent(self, evt):
+##        if (self.cnt == 12):
+##            self.im = self.ax.matshow(self.data)
+##            
+##        
+###        if (self.cnt == 0):
+###            self.cnt = 1
+###            self.im.set_data(self.X)
+###        elif (self.cnt == 1):
+###            self.im.set_data(self.Y)
+###            self.cnt = 0    
+##        elif (self.cnt > 12):    
+##            self.im.set_data(self.data)            
+##            #print self.data[0, 4:10]
+##            
+##            self.im.axes.figure.canvas.draw()
+#        try: 
+#            self.im.set_data(self.data)
 #            self.im.axes.figure.canvas.draw()
-        try: 
-            self.im.set_data(self.data)
-            self.im.axes.figure.canvas.draw()
-        except AttributeError:
-            self.im = self.ax.matshow(self.data)
-            self.im.axes.figure.canvas.draw()
+#        except AttributeError:
+#            self.im = self.ax.matshow(self.data)
+#            self.im.axes.figure.canvas.draw()
+#        
+#        self.cnt += 1
         
-        self.cnt += 1
-        
+
+#    def onClick(self, event):
+#        print 'click: ', event.xdata, event.ydata
+#    
+#    def onRelease(self, event):
+#        print 'release: ', event.xdata, event.ydata
+
     def updateData(self, data, width, height):
-        self.data = np.reshape(data, (height, width))
+        try:
+            self.data = np.reshape(data, (height, width))
+        except ValueError:
+            pass # happens if update data is called before the new width and height are calculated upon changing the image size
         try: 
             self.im.set_data(self.data)
             self.im.axes.figure.canvas.draw()
         except AttributeError:
-            self.im = self.ax.matshow(self.data)
+            self.im = self.ax.matshow(self.data, cmap=cm.hot)#gist_heat)
+            self.ax.axis([self.parent.parent.hstart, self.parent.parent.hend, self.parent.parent.vstart, self.parent.parent.vend])
             self.im.axes.figure.canvas.draw()
-            print 'show yourself!'
+    
+    def onselect(self, eclick, erelease):
+        'eclick and erelease are matplotlib events at press and release'
+        print ' startposition : (%f, %f)' % (eclick.xdata, eclick.ydata)
+        print ' endposition   : (%f, %f)' % (erelease.xdata, erelease.ydata)
+          
+        if (eclick.ydata > erelease.ydata):
+            eclick.ydata, erelease.ydata= erelease.ydata, eclick.ydata
+        if (eclick.xdata > erelease.xdata):
+            eclick.xdata, erelease.xdata = erelease.xdata, eclick.xdata
         
+        # data sent will always have the eclick point be the left point and the erelease point be the right point
+        self.parent.parent.changeImageRegion(int(eclick.xdata), int(eclick.ydata), int(erelease.xdata), int(erelease.ydata))
+    
+    def setupSelector(self):
+        self.rectSelect = RectangleSelector(self.ax, self.onselect, drawtype='box', rectprops = dict(facecolor='red', edgecolor = 'white',
+                 alpha=0.5, fill=False))           
         
 class AppWindow(QtGui.QWidget):
     """Creates the window for the new plot"""
@@ -234,7 +271,7 @@ class AppWindow(QtGui.QWidget):
         
         self.parent = parent
         
-        self.cmon = CPUMonitor(self)
+        self.cmon = Canvas(self)
         self.cmon.show()
         # instantiate the navigation toolbar
         ntb = NavigationToolbar(self.cmon, self)
@@ -260,21 +297,59 @@ class AppWindow(QtGui.QWidget):
         
         abortButton = QtGui.QPushButton("Abort Video", self)
         abortButton.setGeometry(QtCore.QRect(0, 0, 30, 30))
-        abortButton.clicked.connect(self.abortVideo)        
+        abortButton.clicked.connect(self.abortVideo)
+        
+        resetScaleButton = QtGui.QPushButton("Reset Scale", self)
+        resetScaleButton.setGeometry(QtCore.QRect(0, 0, 30, 30))
+        resetScaleButton.clicked.connect(self.resetScale)
+        
+        emGainLabel = QtGui.QLabel()
+        emGainLabel.setText('EM Gain: ')
+        
+        self.emGainSpinBox = QtGui.QSpinBox()
+        self.emGainSpinBox.setMinimum(0)
+        self.emGainSpinBox.setMaximum(255)
+        self.emGainSpinBox.setSingleStep(1)  
+        self.emGainSpinBox.setValue(EMGAIN)     
+        self.emGainSpinBox.setKeyboardTracking(False)
+        self.connect(self.emGainSpinBox, QtCore.SIGNAL('valueChanged(int)'), self.changeEMGain)
+
+        exposureLabel = QtGui.QLabel()
+        exposureLabel.setText('Exposure (ms): ')
+        
+        self.exposureSpinBox = QtGui.QSpinBox()
+        self.exposureSpinBox.setMinimum(0)
+        self.exposureSpinBox.setMaximum(1000)
+        self.exposureSpinBox.setSingleStep(1)  
+        self.exposureSpinBox.setValue(EXPOSURE*1000)     
+        self.exposureSpinBox.setKeyboardTracking(False)
+        self.connect(self.exposureSpinBox, QtCore.SIGNAL('valueChanged(int)'), self.changeExposure)
+
         
          # Layout
-        self.buttonBox = QtGui.QHBoxLayout()
+        self.bottomPanel = QtGui.QHBoxLayout()
         
-        self.buttonBox.addWidget(temperatureButton)
-        self.buttonBox.addWidget(liveVideoButton)
-        self.buttonBox.addWidget(singleButton)
-        self.buttonBox.addWidget(abortButton)
+        self.bottomPanel.addWidget(temperatureButton)
+        self.bottomPanel.addWidget(liveVideoButton)
+        self.bottomPanel.addWidget(abortButton)
+        self.bottomPanel.addWidget(resetScaleButton)
+        self.bottomPanel.addWidget(singleButton)
+        self.bottomPanel.addWidget(emGainLabel)
+        self.bottomPanel.addWidget(self.emGainSpinBox)
+        self.bottomPanel.addWidget(exposureLabel)
+        self.bottomPanel.addWidget(self.exposureSpinBox)
     
-        self.buttonBox.addStretch(0)
-        self.buttonBox.setSizeConstraint(QtGui.QLayout.SetFixedSize)        
-        layout.addLayout(self.buttonBox)
+        self.bottomPanel.addStretch(0)
+        self.bottomPanel.setSizeConstraint(QtGui.QLayout.SetFixedSize)        
+        layout.addLayout(self.bottomPanel)
         
         self.setLayout(layout)
+
+    def changeEMGain(self, value):
+        self.parent.changeEMGain(self.emGainSpinBox.value())
+
+    def changeExposure(self, value):
+        self.parent.changeExposure(float(self.exposureSpinBox.value())/1000) #convert ms to s       
 
     def abortVideo(self, evt):
         self.parent.abortVideo()
@@ -287,17 +362,22 @@ class AppWindow(QtGui.QWidget):
     
     def liveVideo(self, evt):
         self.parent.liveVideo()
+        
+    def resetScale(self, evt):
+        self.parent.changeImageRegion(self.parent.originalHStart,self.parent.originalVStart,self.parent.originalHEnd,self.parent.originalVEnd)
     
     def closeEvent(self, evt):
+        self.parent.abortVideo()
         try:
             self.killTimer(self.cmon.timer)
         except AttributeError:
-            pass            
+            pass 
+        self.parent.reactor.stop()           
 
 class AndorClient():
     def __init__(self, reactor):
         self.reactor = reactor
-        self.live = True
+        self.live = False
         self.openVideoWindow()
         self.connect()
 
@@ -321,28 +401,40 @@ class AndorClient():
         temp = yield self.server.get_current_temperature()
         print temp
 
-        yield self.server.set_trigger_mode(0)
+        try:
+            yield self.server.set_trigger_mode(0)
+        except:
+            print 'client not closed properly'
+            self.abortVideo()
+            yield self.server.set_trigger_mode(0)
         yield self.server.set_read_mode(4)
 
-        yield self.server.set_emccd_gain(255)
-        yield self.server.set_exposure_time(.3)      
+        yield self.server.set_emccd_gain(EMGAIN)
+        yield self.server.set_exposure_time(EXPOSURE)      
         yield self.server.cooler_on()
         
         
-        #imageRegion = yield self.server.get_image_region()
-        self.hstart = 100
-        self.hend = 250
-        self.vstart = 100
-        self.vend = 250
+        self.detectorDimensions = yield self.server.get_detector_dimensions() #this gives a type error?
+
+        # original window size can be changed here
+        self.originalHStart = 1
+        self.originalHEnd = 658#self.detectorDimensions[0]
+        self.originalVStart = 1
+        self.originalVEnd = 496#self.detectorDimensions[1]      
         
-        self.width = self.hend - self.hstart#imageRegion[3]
-        self.height = self.vend - self.vstart#imageRegion[5]
+        self.hstart = self.originalHStart
+        self.hend = self.originalHEnd
+        self.vstart = self.originalVStart
+        self.vend = self.originalVEnd
+        
+        self.width = self.hend - self.hstart
+        self.height = self.vend - self.vstart
         
         print 'width: ', self.width
         print 'height: ', self.height
         
         error = yield self.server.set_image_region(1,1,self.hstart,self.hend,self.vstart,self.vend)
-        print 'image error: ', error
+        print 'image: ', error
         
     @inlineCallbacks
     def printTemperature(self):
@@ -352,20 +444,51 @@ class AndorClient():
     @inlineCallbacks
     def singleScan(self):
         
-        width = self.width
-        height = self.height
+#        width = self.width
+#        height = self.height
+#
+##        fig, ax = plt.subplots()
+#          
+#        print "Ready for Acquisition..."
+#        
+#        status = yield self.server.get_status()
+#        if (status == 'DRV_IDLE'):
+#            yield self.server.set_acquisition_mode(1)
+#            yield self.server.get_acquisition_mode()
+#            yield self.server.start_acquisition()
+#            data = yield self.server.get_acquired_data()
+#            newdata = data.asarray
+#            print 'Acquired'
 
-#        fig, ax = plt.subplots()
-          
-        print "Ready for Acquisition..."
         
-        status = yield self.server.get_status()
-        if (status == 'DRV_IDLE'):
-            yield self.server.set_acquisition_mode(1)
-            yield self.server.get_acquisition_mode()
-            yield self.server.start_acquisition()
-            data = yield self.server.get_acquired_data()
-            newdata = data.asarray
+        width = np.arange(self.width + 1) + 1
+        height = np.arange(self.height + 1) + 1
+
+        lenWidth = len(width)
+        lenHeight = len(height)
+
+        Width = np.ravel([width]*lenHeight)
+
+        #This is slower! and wrong!
+#        Height = np.zeros(lenWidth*lenHeight).reshape(lenHeight, lenWidth)
+#        for i in Width:
+#            for j in np.arange(lenWidth):
+#                #print (i -1), j
+#                Height[(i - 1), j] = i
+#        Height = np.ravel(Height)
+        Height = []
+        for i in width:
+            Height.append([i]*lenHeight)
+        Height = np.ravel(np.array(Height))
+        
+        yield self.cxn.data_vault.cd('Camera Snapshots')
+        t = datetime.now()
+        dateTime = str(t.strftime("%Y-%m-%d %H:%M:%S"))
+#        yield self.cxn.data_vault.new('Half-Life', [('x', 'in')], [('y','','in')])         
+#        yield self.cxn.data_vault.add([[0.0,0.2],[1.0,0.2],[2.4,2.3],[3.3,0.0],[4.7,0.4],[4.5,1.2],[3.8,1.0],[2.3,4.8],[1.1,4.8],[1.1,4.1],[1.7,4.1],[2.0,3.4],[0.0,0.2]] )
+        yield self.cxn.data_vault.new(dateTime, [('Pixels', '')], [('Pixels','',''), ('Counts','','')])         
+        toDataVault = np.array(np.vstack((Height, Width, self.newdata)).transpose(), dtype=float)
+        yield self.cxn.data_vault.add(toDataVault)
 #            newarray = np.reshape(newdata, (height, width))
 #            ax.matshow(newarray)
 #                
@@ -392,7 +515,7 @@ class AndorClient():
             while(self.live == True):
 #                t1 = time.clock()
                 data = yield self.server.get_most_recent_image()
-                newdata = data.asarray
+                self.newdata = data.asarray
 #                print newdata.shape
 #                t2 = time.clock()
 #                print 'Acquisition Time: ', (t2 - t1)
@@ -400,18 +523,49 @@ class AndorClient():
                 #newarray = np.reshape(newdata, (height, width))
                 #print newarray[0, 4:7]
                 #self.data = newarray
-                self.win.cmon.updateData(newdata, (self.width + 1), (self.height + 1))
+                self.win.cmon.updateData(self.newdata, (self.width + 1), (self.height + 1))
 #                if (cnt == 0):
 #                    self.win.cmon.startYourEngines()
 #                    cnt += 1
             yield self.server.abort_acquisition()
+            
+    @inlineCallbacks
+    def changeEMGain(self, value):
+        yield self.server.set_emccd_gain(value)
+
+    @inlineCallbacks
+    def changeExposure(self, value):
+        if (self.live == True):
+            self.abortVideo()
+            yield self.server.set_exposure_time(value)
+            self.liveVideo()
+        else:
+            yield self.server.set_exposure_time(value)
+       
     
     @inlineCallbacks
     def abortVideo(self):
         self.live = False
         yield self.server.abort_acquisition()
         print 'aborted'
-
+        
+    @inlineCallbacks
+    def changeImageRegion(self, xleft, yleft, xright, yright):
+        self.hstart = xleft
+        self.hend = xright
+        self.vstart = yleft
+        self.vend = yright
+        self.width = self.hend - self.hstart
+        self.height = self.vend - self.vstart
+        if (self.live == True):
+            self.abortVideo()
+            error = yield self.server.set_image_region(1,1,self.hstart,self.hend,self.vstart,self.vend)
+            print 'image: ', error
+            self.liveVideo()
+        else:
+            error = yield self.server.set_image_region(1,1,self.hstart,self.hend,self.vstart,self.vend)
+            print 'image error: ', error     
+    
 if __name__ == "__main__":
     a = QtGui.QApplication( [] )
     import qt4reactor
