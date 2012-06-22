63ef52402a7b0a803cefda813cbc71163e016d71
Firstname <haeffnerlab@gmail.com>
2012-06-22 10:10:11 -0700

    reCrystallize redux

--- lattice/scripts/scriptLibrary/reCrystallize.py
+++ lattice/scripts/scriptLibrary/reCrystallize.py
@@ -1,50 +1,126 @@
-
-import labrad
-import numpy
+#import labrad
+#import numpy
+#import time
+#from msvcrt import getch, kbhit
+#
+#cxn = labrad.connect()
+#cxnlab = labrad.connect('192.168.169.49') #connection to labwide network
+#rf = cxn.trap_drive
+#dc = cxn.dc_box
+#ld = cxnlab.laserdac
+#pulser = cxn.pulser
+#
+#def kbfunc():
+#    x = kbhit()
+#    if x:
+#        ret = getch()
+#    else:
+#        ret = 0
+#    return ret
+#
+#ch = '397'
+#initcavity = ld.getvoltage(ch) 
+#initpower = rf.amplitude()
+#min = initcavity - 20
+#
+#print 're-crystallization script running..'
+#print initcavity, min, initpower
+#
+#while 1:
+#	key = kbfunc()
+#	#initpower = rf.amplitude()
+#	if key == "\r": #\x1b
+#    		print 'Resetting rf & Switching on far-red beam..'		
+#		time.sleep(0.1)
+#    		rf.amplitude(-7.0)
+#		pulser.switch_manual('crystallization',  True) # crystallization shutter open
+#		pulser.switch_manual('110DP',  False) # 110DP off 
+#		#dc.setendcap(1,0.695)
+#		#dc.setendcap(2,5.305)
+#		time.sleep(2)	
+#		pulser.switch_manual('110DP',  True)
+#		#pulser.switch_manual('crystallization',  False)
+#		rf.amplitude(initpower)
+#		#dc.setendcap(1,3.695)
+#		#dc.setendcap(2,8.305)
+#   		#for voltage in numpy.arange(initcavity, min, -1):
+#   		 #   print voltage
+#   		 #   time.sleep(0.05)
+#   		 #   ld.setvoltage(ch,voltage)
+# 
 import time
-from msvcrt import getch, kbhit
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.threads import deferToThread
+from PyQt4 import QtGui, QtCore
 
-cxn = labrad.connect()
-cxnlab = labrad.connect('192.168.169.49') #connection to labwide network
-rf = cxn.trap_drive
-dc = cxn.dc_box
-ld = cxnlab.laserdac
-pulser = cxn.pulser
+class ShortCutter(QtGui.QWidget):
+    def __init__(self,reactor, parent=None):
+        QtGui.QWidget.__init__(self)
+        self.reactor = reactor
+        self.connect()
+        
+    @inlineCallbacks
+    def connect(self):
+        from labrad.wrappers import connectAsync
+        self.cxn = yield connectAsync()
+        self.rf = self.cxn.trap_drive
+        self.pulser = self.cxn.pulser
+        
+        self.initpower = yield self.rf.amplitude()
 
-def kbfunc():
-    x = kbhit()
-    if x:
-        ret = getch()
-    else:
-        ret = 0
-    return ret
+        self.setupWidget()
+        
+    def setupWidget(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.grid = QtGui.QGridLayout()
+        self.grid.setSpacing(5)
+        
+        singleButton = QtGui.QPushButton("Recrystalize!", self)
+        singleButton.setGeometry(QtCore.QRect(0, 0, 30, 30))
+        singleButton.clicked.connect(self.recrystallize)
+        
+        self.shortcut = QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_R), self)
+        self.shortcut.activated.connect(self.recrystallize)
+        
+        shortcutLabel = QtGui.QLabel()
+        shortcutLabel.setText('Ctrl-r')
+ 
+        self.grid.addWidget(singleButton, 0, 0, QtCore.Qt.AlignCenter)
+        self.grid.addWidget(shortcutLabel, 0, 1, QtCore.Qt.AlignCenter)
+        self.setLayout(self.grid)
+        self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        
+        self.show()
 
-ch = '397'
-initcavity = ld.getvoltage(ch) 
-initpower = rf.amplitude()
-min = initcavity - 20
+    @inlineCallbacks    
+    def recrystallize(self, evt=1):
+        print 'Resetting rf & Switching on far-red beam..'        
+        yield deferToThread(time.sleep, 0.1)
+        yield self.rf.amplitude(-7.0)
+        yield self.pulser.switch_manual('crystallization',  True) # crystallization shutter open
+        yield self.pulser.switch_manual('110DP',  False) # 110DP off 
+        #dc.setendcap(1,0.695)
+        #dc.setendcap(2,5.305)
+        yield deferToThread(time.sleep, 2.0)  
+        yield self.pulser.switch_manual('110DP',  True)
+        #pulser.switch_manual('crystallization',  False)
+        yield self.rf.amplitude(self.initpower)
+        #dc.setendcap(1,3.695)
+        #dc.setendcap(2,8.305)
+           #for voltage in numpy.arange(initcavity, min, -1):
+            #   print voltage
+            #   time.sleep(0.05)
+            #   ld.setvoltage(ch,voltage)
 
-print 're-crystallization script running..'
-print initcavity, min, initpower
+    
+    def closeEvent(self, x):
+        self.reactor.stop()
 
-while 1:
-	key = kbfunc()
-	#initpower = rf.amplitude()
-	if key == "\r": #\x1b
-    		print 'Resetting rf & Switching on far-red beam..'		
-		time.sleep(0.1)
-    		rf.amplitude(-7.0)
-		pulser.switch_manual('crystallization',  True) # crystallization shutter open
-		pulser.switch_manual('110DP',  False) # 110DP off 
-		#dc.setendcap(1,0.695)
-		#dc.setendcap(2,5.305)
-		time.sleep(2)	
-		pulser.switch_manual('110DP',  True)
-		#pulser.switch_manual('crystallization',  False)
-		rf.amplitude(initpower)
-		#dc.setendcap(1,3.695)
-		#dc.setendcap(2,8.305)
-   		#for voltage in numpy.arange(initcavity, min, -1):
-   		 #   print voltage
-   		 #   time.sleep(0.05)
-   		 #   ld.setvoltage(ch,voltage)
+if __name__=="__main__":
+    a = QtGui.QApplication( [] )
+    import qt4reactor
+    qt4reactor.install()
+    from twisted.internet import reactor
+    ShortCutter = ShortCutter(reactor)
+    #ShortCutter.show()
+    reactor.run()
