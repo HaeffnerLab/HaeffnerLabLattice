4417cd9dd8e3f343d4b2941d41147bfd9f192453
Hlab, lab-197 <haeffnerlab@gmail.com>
2012-07-25 20:26:44 -0700

    pulse sequence works with both pulsers

--- lattice/scripts/PulseSequences/sample_729.py
+++ lattice/scripts/PulseSequences/sample_729.py
@@ -9,24 +9,29 @@ class sample_729(Sequence):
     def defineSequence(self):   
         p = self.parameters
         pulser = self.pulser
+        pulser729 = self.pulser729
         #caluclate time intervals
         self.recordTime =  p.record  
         pulser.add_ttl_pulse('TimeResolvedCount', 0.0 ,  self.recordTime )
+        pulser729.add_dds_pulses('729DP',[(0.1 , 220.0 , -33.0, 0.0)])
+        pulser729.add_dds_pulses('729DP',[(0.5 , 210.0 , -33.0, 0.0)])
         
 if __name__ == '__main__':
     import labrad
     cxn = labrad.connect()
     cxnlab = labrad.connect('192.168.169.49')
     pulser = cxn.pulser
-    print cxnlab
-    seq = sample_729(pulser)
+    pulser729 = cxnlab.pulser_729
+    seq = sample_729([pulser, pulser729])
     pulser.new_sequence()
+    pulser729.new_sequence()
     params = {
-                'record':100*10**-3,
+                'record':1000*10**-3,
               }
     seq.setVariables(**params)
     seq.defineSequence()
     pulser.program_sequence()
+    pulser729.program_sequence()
     pulser.reset_timetags()
     pulser.start_single()
     pulser.wait_sequence_done()
