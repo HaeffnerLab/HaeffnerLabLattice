9627d09cabc7cc03e23a73f803d51c5f71960c3f
Firstname <haeffnerlab@gmail.com>
2012-08-20 11:57:06 -0700

    space in suffix

--- lattice/clients/scriptcontrol/globalgrid.py
+++ lattice/clients/scriptcontrol/globalgrid.py
@@ -192,7 +192,7 @@ class GlobalGrid(QtGui.QTableWidget):
     @inlineCallbacks
     def updateSpinBoxValueToSemaphore(self, parameterValue):
         from labrad import types as T       
-        yield self.parent.server.set_parameter(self.globalParameterDict[self.doubleSpinBoxParameterDict[self.sender()]], [self.sender().minimum(), self.sender().maximum(), T.Value(parameterValue, str(self.sender().suffix()))], context = self.context)
+        yield self.parent.server.set_parameter(self.globalParameterDict[self.doubleSpinBoxParameterDict[self.sender()]], [self.sender().minimum(), self.sender().maximum(), T.Value(parameterValue, str(self.sender().suffix()[1:]))], context = self.context)
 
     @inlineCallbacks
     def updateLineEditValueToSemaphore(self):
