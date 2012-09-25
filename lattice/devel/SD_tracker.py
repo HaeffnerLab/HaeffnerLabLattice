29e6162b040e4944875e10f3965656663f53ef2d
Firstname <haeffnerlab@gmail.com>
2012-09-24 18:37:08 -0700

    more sd tracking

--- lattice/devel/SD_tracker.py
+++ lattice/devel/SD_tracker.py
@@ -27,19 +27,19 @@ class EnergyLevel():
         S = spin_s
         self.L = L = angular_momentum_l
         J = total_angular_momentum_j
-        self.g = self.lande_factor(S, L, J)
+        lande_factor =  self.lande_factor(S, L, J)
         #sublevels are found, 2* self.J is always an integer, so can use numerator
         self.sublevels_m =  [-J + i for i in xrange( 1 + (2 * J).numerator)]
+        self.energy_scale = (lande_factor * self.bohr_magneton / U.hplanck) #1.4 MHz / gauss
     
     def lande_factor(self, S, L ,J):
         '''computes the lande g factor'''
         g = Fraction(3,2) + Fraction( S * (S + 1) - L * (L + 1) ,  2 * J*(J + 1))
         return g
     
-    def energies(self, B):
-        energy_scale = (self.g * self.bohr_magneton * B.inUnitsOf('T') / U.hplanck).inUnitsOf('MHz')
-        #energy_scale is 1.4 MHz per Gauss
-        energies = [energy_scale * m for m in self.sublevels_m]
+    def magnetic_to_energy(self, B):
+        #given the magnitude of the magnetic field, returns all energies of all zeeman sublevels
+        energies = [(self.energy_scale * m * B.inUnitsOf('T')).inUnitsOf('MHz') for m in self.sublevels_m]
         representations = [self.frac_to_string(m) for m in self.sublevels_m]
         return zip(self.sublevels_m,energies,representations)
     
@@ -64,13 +64,20 @@ class Transitions_SD():
     def get_transition_energies(self):
         if self.B is None: raise Exception ("Magnetic Field Not Specified")
         ans = []
-        for m_s,E_s,repr_s in self.S.energies(self.B):
-            for m_d,E_d,repr_d in self.D.energies(self.B):
+        for m_s,E_s,repr_s in self.S.magnetic_to_energy(self.B):
+            for m_d,E_d,repr_d in self.D.magnetic_to_energy(self.B):
                 if abs(m_d-m_s) in self.allowed_transitions:
                     name = repr_s + repr_d
                     diff = E_d - E_s
                     ans.append((name, diff))
         return ans
+    
+    def energies_to_magnetic_field(self, energies):
+        #given two points in the form ((-1/2,5+/2, 1.0 MHz), (-1/2, 5+/2, 2.0 MHz)), calculates the magnetic field and the zero-field transition frequency
+        s_energy_scale = self.S.energy_scale
+        d_energy_scale = self.D.energy_scale
+        
+        
 
 transitions = Transitions_SD()
 transitions.set_magnetic_field(T.Value(100, 'uT'))
