85037a806012185ece28817644db135aee03174f
Hlab, lab-197 <haeffnerlab@gmail.com>
2012-10-31 16:24:45 -0700

    changes to gui script control experimnets

--- lattice/clients/guiscriptcontrol/experiments/Test2.py
+++ lattice/clients/guiscriptcontrol/experiments/Test2.py
@@ -1,8 +1,6 @@
 import labrad
-import math
 import time
-from numpy  import *
-
+import numpy as np
 
 class Test2():
     def __init__(self):
@@ -10,34 +8,21 @@ class Test2():
         print 'Initializing Test2'
        
     def run(self):
-        
-        ###### Add Data!!
-        
-        y1 = [None] * 200
-        
-        x = arange(24.9, 25.1, .005)
-        def generateData():
-            for i in range(len(x)):
-                p = [.01, 25, .1, 0] #p = [gamma, center, I, offset]
-                y1[i] = p[3] + p[2]*(p[0]**2/((x[i] - p[1])**2 + p[0]**2))# Lorentzian
-                
-        generateData()
+        #generate lorentzian data
+        p = [.01, 25, .1, 0] #p = [gamma, center, I, offset]
+        x = np.arange(24.9, 25.1, .005)
+        y = p[3] + p[2]*(p[0]**2/((x - p[1])**2 + p[0]**2))# Lorentzian
+        #add data to data vault
         self.cxn = labrad.connect()
         self.cxn.server = self.cxn.data_vault
         self.cxn.data_vault.cd('Sine Curves')
         self.cxn.data_vault.new('Lorentzian', [('x', 'num')], [('y1','Test-Spectrum','num')])
         self.cxn.data_vault.add_parameter('Window', ['Lorentzian'])
         self.cxn.data_vault.add_parameter('plotLive', True)
-        for i in range(len(x)):
-            self.cxn.data_vault.add([x[i], y1[i]])
-            data = [i, y1[i]]
-            print data
-            time.sleep(.01)
+        self.cxn.data_vault.add(np.vstack((x,y)).transpose())
+        time.sleep(1)
         self.cxn.data_vault.add_parameter('Fit', ['[]', 'Lorentzian', '[0.01, 25.0, 0.10000000000000001, 1.4901161193880158e-08]']) 
-        
-        ###############################
-        
-        # now we play the waiting game
+        #wait until the fit is accepted
         i = 0
         while(i < 200): # timeout
             try:
@@ -48,7 +33,7 @@ class Test2():
                 self.doCalculation()
                 break
             else:
-                time.sleep(.5)
+                time.sleep(1.0)
             i += 1
         self.cleanUp()
         
