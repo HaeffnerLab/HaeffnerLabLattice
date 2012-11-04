9862e887cd76c526277ea4d917423e0849f96fb3
Hlab, lab-197 <haeffnerlab@gmail.com>
2012-11-04 14:33:20 -0800

    new analysis of branching ratio

---
+++ lattice/scripts/dataAnalysis/BranchingRatio/2012Nov02/bin.py
@@ -0,0 +1,41 @@
+import numpy as np
+import labrad
+from constants import constants as c
+
+#get timatags from the npy file
+data = np.load(c.timetag_np_file)
+iterations = data[:,0]
+timetags = data[:,1]
+iters, indices = np.unique(iterations, return_index = True)
+split = np.split(timetags, indices[1:])
+sums = np.array([tags.size for tags in split])
+#finding out when we lost the ion
+#print 'start'
+#for s in sums: print s
+#print 'counts when the ion was lost'
+#differences = np.ediff1d(sums)
+#print sums[(differences.argmin() - 5):(differences.argmin() + 5)] 
+threshold = 18000.0
+no_ion_iter = np.where(sums < threshold)[0].min() + 1 #cut off at first point that's below the threshold
+#cut the iterations and timetags arrays to when we had the ion
+iterations = iterations[np.where(iterations < iters[no_ion_iter])]
+timetags = timetags[np.where(iterations < iters[no_ion_iter])]
+print 'Completed {} iterations before losing ion'.format(iterations.max() + 1)
+
+cxn = labrad.connect()
+dv = cxn.data_vault
+dv.cd(c.datavault_dir)
+dv.open(1)
+##will get these from data vault parameters for future data
+start_recording_timetags = dv.get_parameter('start_recording_timetags')
+cycle_time = dv.get_parameter('timetag_record_cycle')
+bin_size = 10e-9
+timetags = (timetags - start_recording_timetags) % cycle_time
+bin_edges = 1 + cycle_time / bin_size
+bins = np.linspace(0,cycle_time, bin_edges)
+hist = np.histogram(timetags, bins)[0]
+#save the binned data
+together = np.vstack((bins[:-1], hist)).transpose()
+bin_filename = c.bin_filename
+np.save(bin_filename, together)
+print 'DONE BINNING'
