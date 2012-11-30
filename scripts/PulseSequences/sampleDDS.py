5dddeed4923e65749ba6da4bb80e44c0697fb747
Hlab, lab-197 <haeffnerlab@gmail.com>
2012-11-30 15:36:19 -0800

    changed DDS sample

--- scripts/PulseSequences/sampleDDS.py
+++ scripts/PulseSequences/sampleDDS.py
@@ -1,5 +1,5 @@
 from PulseSequence import PulseSequence
-from labrad import types as T
+from labrad.units import WithUnit
 
 class sampleDDS(PulseSequence):
     #dictionary of variable: (type, min, max, default)
@@ -8,20 +8,22 @@ class sampleDDS(PulseSequence):
                   ]
         return config
     
-
     def sequence(self):
-        duration = T.Value(3000, 'ms')
-        freqs = [T.Value(f, 'MHz') for f in [210.0, 240.0, 230.0]]
-        for i,f in enumerate(freqs):
-            self.dds_pulses.append( ('729DP',self.start + 2*i*duration + T.Value(10, 'us'), duration, f, T.Value(-3.0, 'dBm')) )
-        
+        start_first = WithUnit(10, 'us')
+        on_time = WithUnit(100, 'ms')
+        off_time = WithUnit(20, 'us')
+        start_second = start_first + on_time + off_time
+        freq = WithUnit(150.0, 'MHz')
+        ampl = WithUnit(-3.0, 'dBm')
+        self.dds_pulses.append( ('729DP', start_first, on_time, freq, ampl) )
+        self.dds_pulses.append( ('729DP', start_second, on_time, freq, ampl) )
         
 if __name__ == '__main__':
     import labrad
     cxn = labrad.connect()
     cs = sampleDDS(**{})
     cs.programSequence(cxn.pulser)
-    cxn.pulser.start_number(3)
+    cxn.pulser.start_number(10)
     cxn.pulser.wait_sequence_done()
     cxn.pulser.stop_sequence()
     print 'DONE'
