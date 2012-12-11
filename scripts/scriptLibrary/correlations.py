20c7e0e22c4321667c511bdd675cd5283647231c
Hlab, lab-197 <haeffnerlab@gmail.com>
2012-12-11 10:48:34 -0800

    updated correlations

--- scripts/scriptLibrary/correlations.py
+++ scripts/scriptLibrary/correlations.py
@@ -1,68 +1,42 @@
 import numpy as np
 import matplotlib
-matplotlib.use('Qt4Agg')
+#matplotlib.use('Qt4Agg')
 from matplotlib import pyplot
 
 class correlator(object):
-    
+      
     @staticmethod
-    def element_differences(arr, bins):
+    def g2(intensity, resolution = 1, compute_range = None):
         '''
-        computes differences between all pairs of elements from an array of timetags
-        
-        maximum correlation is one third of the timetag duration. 
-        this way, time differences are computed from the the middle third of timetags never lie outside the given range. 
+        computes g2 from the intensity array
+        @var arr is the intensity array of intesnity per unit time
+        @var time resolution of the intensity array
         '''
-        arr = np.array(arr)
-        arr_length = arr.max() - arr.min()
-        arr_center = arr.min() + arr_length / 2.0
-        max_correlation_length = arr_length / 3.0
-        #correlate_window are timetags with which all differences are computed. 
-        #correlate_window is max_correlation_length-wide. centered in the middle of timetags
-        start_correlate_window = (arr_center - max_correlation_length/2.0)
-        stop_correlate_window = (arr_center + max_correlation_length/2.0)
-        correlate_where = (arr >= start_correlate_window) * (arr <= stop_correlate_window)
-        correlate_window = arr[correlate_where]
-        #rearangeing all timetags so that they start with timteags from correlate_window. this helps with indexing.
-        all_timetags = list(correlate_window)
-        all_timetags.extend(arr[np.logical_not(correlate_where)])
-        differences = []
-        for i in range(len(correlate_window)):
-            for j in range(len(all_timetags)):
-                diff = correlate_window[i] - all_timetags[j]
-                if 0 <= diff < max_correlation_length:
-                    differences.append(diff)
-        return differences
-
+        intensity = np.array(intensity)
+        N = intensity.size
+        max_correlation_length = N / 3
+        correlate_window = intensity[N/3 : 2*N/3 + 1] #middle third of the array
+        g2 = []
+        kk = []
+        if compute_range == None:
+            range_k = range(0, max_correlation_length)
+        else:
+            range_k = range(compute_range[0], compute_range[1])
+        for k in range_k:
+            #for each correlation length, offset the array and compute the product
+            offset = intensity[N/3 + k: 2*N/3 + 1 + k]
+            norm = max_correlation_length / float(np.sum(correlate_window) * np.sum(offset)) 
+            prod = norm * np.dot(correlate_window, offset)
+            g2.append(prod)
+            kk.append(k)
+        return np.array(kk), np.array(g2)    
+                
 if __name__ == '__main__':
     #g2 for a poisonnian process
-    count_rate = 0.01
-    samples = 30000.0
+    count_rate = 0.2
+    samples = 2000.0
     photon_numbers = np.random.poisson(lam = count_rate, size = samples) #photon number is poisonian
-    
-    timetags = []
-    for i,num in enumerate(photon_numbers):
-        if num > 0:
-            timetags.extend(num * [i])
-    differences = correlator.element_differences(timetags, 500)
-    
-#    average =  np.mean(photon_numbers)
-#    print np.bincount(photon_numbers)
-#    print 'average', average
-#    
-#    print np.mean(photon_numbers**2) / float(np.mean(photon_numbers)**2)
-    
-    from correlations_2 import correlator as corr2
-    
-    ks, result =  corr2.g2(photon_numbers, compute_range = None)
-    
-
+    ks, result =  correlator.g2(photon_numbers, compute_range = None)
     pyplot.figure()
     pyplot.plot(ks, result)
-    ct = np.bincount(differences)
-    pyplot.plot(ct / np.mean(ct))
     pyplot.show()
-    hist,bins = correlator.element_differences(timetags, 500)
-    pyplot.bar(bins[:-1], hist, width = bins[1]-bins[0])
-    pyplot.show()
-#    g2 for a poisonnian process with 
