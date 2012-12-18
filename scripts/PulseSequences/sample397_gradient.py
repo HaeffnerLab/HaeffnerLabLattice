9c896000e027cbca671c4451adfcfe3f3cd5eb28
thetorque <thanedp@berkeley.edu>
2012-12-17 17:31:39 -0800

    modified 397 gradient sample sequence

--- scripts/PulseSequences/sample397_gradient.py
+++ scripts/PulseSequences/sample397_gradient.py
@@ -11,11 +11,11 @@ class sampleDDS(PulseSequence):
     def sequence(self):
         freq = WithUnit(110.0, 'MHz')
         self.dds_pulses.append( ('110DP', WithUnit(10, 'us'), WithUnit(5, 'us'), freq, WithUnit(-61.0, 'dBm')) )
-        self.dds_pulses.append( ('110DP', WithUnit(10, 'us'), WithUnit(5, 'us'), freq, WithUnit(-51.0, 'dBm')) )
-        self.dds_pulses.append( ('110DP', WithUnit(15, 'us'), WithUnit(5, 'us'), freq, WithUnit(-41.0, 'dBm')) )
-        self.dds_pulses.append( ('110DP', WithUnit(20, 'us'), WithUnit(5, 'us'), freq, WithUnit(-31.0, 'dBm')) )
-        self.dds_pulses.append( ('110DP', WithUnit(25, 'us'), WithUnit(5, 'us'), freq, WithUnit(-21.0, 'dBm')) )
-        self.dds_pulses.append( ('110DP', WithUnit(30, 'us'), WithUnit(200, 'us'), freq, WithUnit(-11.0, 'dBm')) )
+        self.dds_pulses.append( ('110DP', WithUnit(15, 'us'), WithUnit(5, 'us'), freq, WithUnit(-51.0, 'dBm')) )
+        self.dds_pulses.append( ('110DP', WithUnit(20, 'us'), WithUnit(5, 'us'), freq, WithUnit(-41.0, 'dBm')) )
+        self.dds_pulses.append( ('110DP', WithUnit(25, 'us'), WithUnit(5, 'us'), freq, WithUnit(-31.0, 'dBm')) )
+        self.dds_pulses.append( ('110DP', WithUnit(30, 'us'), WithUnit(5, 'us'), freq, WithUnit(-21.0, 'dBm')) )
+        self.dds_pulses.append( ('110DP', WithUnit(35, 'us'), WithUnit(200, 'us'), freq, WithUnit(-11.0, 'dBm')) )
         
 if __name__ == '__main__':
     import labrad
