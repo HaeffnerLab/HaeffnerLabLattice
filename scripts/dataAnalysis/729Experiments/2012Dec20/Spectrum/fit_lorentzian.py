f4f9b6254d22c6011a46073d77c5dbe7a556333a
Hlab, lab-197 <haeffnerlab@gmail.com>
2013-01-14 17:21:47 -0800

    lorentzian fit with error bars

--- scripts/dataAnalysis/729Experiments/2012Dec20/Spectrum/fit_lorentzian.py
+++ scripts/dataAnalysis/729Experiments/2012Dec20/Spectrum/fit_lorentzian.py
@@ -9,43 +9,69 @@ from scipy.stats import chi2
 #get access to servers
 cxn = labrad.connect()
 dv = cxn.data_vault
-
 #change directory
-
-figure = pyplot.figure()
-dv.cd(['','Experiments','729Experiments','Spectrum','2012Dec20','2158_40'])
+directory = ('2012Dec20','2158_40')
+dv.cd(['','Experiments','729Experiments','Spectrum',directory[0],directory[1]])
 dv.open(1)
 data = dv.get().asarray
-x = (data[:,0]-226.589)*1000*2
-y = data[:,1]
-err = np.ones_like(y)*0.05
+#get parameters
+line = dv.get_parameter('spectrum_saved_frequency')
+excitation_time = dv.get_parameter('excitation_time')[2]['us']
 
-n = len(x) # the number of data points
-p0 = [0, 3,0.5] # initial values of parameters
-f = lambda x, x_0, b, a: a*b*(1/(2*np.pi))/((b/2)**2+(x-x_0)**2) # define the function to be fitted
+freqs = 2 * 1000 * data[:,0] #frequency in real KHz
+probs = data[:,1]
+trials = 100
 
-p, covm = curve_fit(f, x, y, p0, err) # do the fit
-x_0,b,a = p
-chisq = sum(((f(x, x_0,b,a) - y)/err)**2) # compute the chi-squared
+def errorBarSimple(trials, prob):
+    #look at wiki http://en.wikipedia.org/wiki/Checking_whether_a_coin_is_fair
+    '''returns 1 sigma error bar on each side i.e 1 sigma interval is val - err < val + err'''
+    Z = 1.0
+    s = np.sqrt(prob * (1.0 - prob) / float(trials))
+    err = Z * s
+    return err
+
+err = errorBarSimple(trials, probs)
+
+n = len(freqs) # the number of data points
+p0 = [453177.0, 0.5, 0.5] # initial values of parameters
+
+def lorentzian(w, center, linewidth, area):
+    '''
+    lorentzian function
+    @param center: center of the function
+    @param param:  linewidth \Gamma
+    @param area: total area under the function (1.0 for normalized)
+    '''
+    return (area / np.pi) * (linewidth / 2.0)**2 / ( ((linewidth / 2.0)**2) + (w - center)**2  )
+
+p, covm = curve_fit(lorentzian, freqs, probs, p0, err) # do the fit
+center, linewidth , area = p
+
+
+chisq = sum(((lorentzian(freqs, center, linewidth, area) - probs) / err)**2) # compute the chi-squared
 ndf = n -len(p) # no. of degrees of freedom
 Q = 1. - chi2.cdf(chisq, ndf) # compute the quality of fit parameter Q
 chisq = chisq / ndf # compute chi-squared per DOF
 x_0_err,b_err,a_err = np.sqrt(np.diag(covm)/chisq) # correct the error bars
-print x_0,b,a
-print x_0_err,b_err,a_err
-print chisq
+
+#make plot
+centered_freqs = freqs - center
+figure = pyplot.figure()
 
 
-x = x-x_0
-pyplot.plot(x,y, 'ko')
-x_fit = np.linspace(x.min(), 4*x.max(), 5000)
-pyplot.plot(x_fit-x_0,f(x_fit,x_0,b,a),'r-',linewidth=3.0)
+pyplot.errorbar(centered_freqs, probs, yerr = np.vstack((err,err)), fmt = 'ko')
 
-pyplot.annotate('FWHM = {0:.3f} kHz'.format(b), xy=(0.60, 0.8), xycoords='axes fraction',fontsize=15)
+x_fit = np.linspace(4 * centered_freqs.min(), 4*centered_freqs.max(), 5000)
+pyplot.plot(x_fit, lorentzian(x_fit + center, center ,linewidth , area),'r-',linewidth=3.0)
+text = r'$\Gamma$ = {0:.2f} kHz'.format(linewidth) + '\n'
+text+= 'Experiment {0}, {1}\n'.format(*directory)
+text+= 'Transition {0}\n'.format(line)
+text+= r'Excition time = {0} $\mu s$'.format(excitation_time)
 
+pyplot.annotate(text, xy=(0.7, 0.9), xycoords='axes fraction',fontsize=15)
 pyplot.xlabel( 'Frequency (kHz)',fontsize=15)
 pyplot.ylabel('Excitation probability',fontsize=15)
-#pyplot.title('Excitation spectrum of 729 nm transition',fontsize=20)
+pyplot.title('Excitation spectrum of 729 nm transition',fontsize=20)
 pyplot.ylim([0,1.0])
 pyplot.xlim([-1.5,1.5])
 pyplot.show()
