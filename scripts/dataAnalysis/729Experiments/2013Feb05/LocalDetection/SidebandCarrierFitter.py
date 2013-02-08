2606aa5f0fa473bf78861e1bac2a7befd19b43cc
Firstname <haeffnerlab@gmail.com>
2013-02-07 23:04:39 -0800

    new analysis on dephasing

--- scripts/dataAnalysis/729Experiments/2013Feb05/LocalDetection/SidebandCarrierFitter.py
+++ scripts/dataAnalysis/729Experiments/2013Feb05/LocalDetection/SidebandCarrierFitter.py
@@ -44,7 +44,6 @@ class rabi_flop():
         self.n = np.arange(0, nmax +1) #how many vibrational states to consider
         self.eta = self.lamb_dicke() / np.sqrt(ionnumber)
         self.rabi_coupling = self.rabi_coupling()
-        print self.rabi_coupling
         
     def rabi_coupling(self):
         eta = self.eta
@@ -71,7 +70,7 @@ class rabi_flop():
         eta = k*np.sqrt(hbar/(2*mass*2*np.pi*frequency))*np.abs(np.cos(theta*2.*np.pi / 360.0))
         return eta
         
-    def compute_state_evolution(self, nbar, delta, T_Rabi, t):
+    def compute_state_evolution(self, nbar, delta, f_Rabi, t):
         '''returns the state evolution for temperature nbar, detuning delta, rabi frequency T_Rabi for times t'''
         n = self.n
         if 5 * nbar > self.n.max():
@@ -84,10 +83,9 @@ class rabi_flop():
         nplus=0
         if sideband<0:
             nplus=-sideband
-            print nplus
         
         p = ((float(nbar)/(nbar+1.))**(n+nplus))/(nbar+1.) 
-        result = np.outer(p*omega/np.sqrt(omega**2+delta**2), ones) * (np.sin( np.outer( np.sqrt(omega**2+delta**2)*np.pi/T_Rabi, t ))**2)
+        result = np.outer(p*omega/np.sqrt(omega**2+delta**2), ones) * (np.sin( np.outer( np.sqrt(omega**2+delta**2)*f_Rabi, t ))**2)
         result = np.sum(result, axis = 0)
         return result
 
@@ -95,10 +93,13 @@ cxn = labrad.connect('192.168.169.197')
 dv = cxn.data_vault
 
 pump_eff = 1.0
-offset_time = 0
+offset_time = 600e-9
+sideband_order = 0
+trap_frequency = T.Value(2.8, 'MHz') #Hz
+projection_angle = 45 #degrees
 #heating times in ms
-info = ('Sideband', 0, 0.0, ('2013Feb05','1435_40'), {})
-nbar = Parameter(7.86361467661); delta = 0; T_Rabi = Parameter(4.98366602991e-06);
+info = ('Carrier Rabi-Flop after improving the imaging system', 0.0, ([['2013Feb05','1137_00'],['2013Feb05','1138_55']]), {})
+nbar = Parameter(5); delta = 0; f_Rabi = Parameter(2.*np.pi/26e-6);
 #info = ('Carrier', [(10.0,[('2012Aug20','2314_26')])], {})
 #nbar = 80; delta = 0.00; T_Rabi = 17.0e-6;
 #info = ('Carrier', [(20.0,[('2012Aug20','2316_49')])], {})
@@ -108,46 +109,49 @@ nbar = Parameter(7.86361467661); delta = 0; T_Rabi = Parameter(4.98366602991e-06
 #info = ('Carrier', 0, 50.0, ('2012Aug20','2321_53'), {})
 #nbar = Parameter(270); delta = Parameter(0.15); T_Rabi = Parameter(21.0e-6);
 
-trap_frequency = T.Value(2.8, 'MHz') #Hz
-projection_angle = 45 #degrees
-sideband_order = -1
 flop = rabi_flop(trap_frequency = trap_frequency, projection_angle = projection_angle, sideband_order = sideband_order)
 
 def f(x): 
-    evolution = flop.compute_state_evolution( nbar(), delta, T_Rabi(), x  )
+    evolution = flop.compute_state_evolution( nbar(), delta, f_Rabi(), x  )
     return evolution
 
 
 #plots the data for all waiting times while averaging the probabilities
+times=[]
+prob=[]
 fig = pyplot.figure()
-title,order, wait_time,dataset,kwargs = info 
-date,datasetName = dataset
-dv.cd( ['','Experiments','729Experiments','RabiFlopping',date,datasetName] )
-dv.open(1)  
-times,prob = dv.get().asarray.transpose()
+title, wait_time,dataset,kwargs = info
+for i in range(len(dataset)):
+    date,datasetName = dataset[i]
+    dv.cd( ['','Experiments','729Experiments','RabiFlopping',date,datasetName] )
+    dv.open(1)  
+    addto_times,addto_prob = dv.get().asarray.transpose()
+    times=np.concatenate((times,addto_times))
+    prob=np.concatenate((prob,addto_prob))
+#    print i,len(times)
+    
 tmin,tmax = times.min(), times.max()
-detailed_times = np.linspace(tmin, tmax, 1000) 
+detailed_times = np.linspace(tmin, tmax, 1000)
 #evolution = flop.compute_state_evolution( nbar(), delta, T_Rabi, detailed_times - offset_time )
 #pyplot.plot(detailed_times , evolution,  'b', label = 'fit')
 
-fitting_region = np.where(times <= tmax)
-p,success = fit(f, [nbar, T_Rabi], y = prob[fitting_region], x = times[fitting_region] - offset_time)
+fitting_region = np.where(times <= 200e-6)
+p,success = fit(f, [nbar, f_Rabi], y = prob[fitting_region], x = times[fitting_region] - offset_time)
 print 'fit for nbar is', nbar()
-print 'fit to T rabi is ', T_Rabi()
-print 'Rabi Frequency is ', 2.*np.pi/T_Rabi()
-evolution = flop.compute_state_evolution( nbar(), delta, T_Rabi(), detailed_times - offset_time )
-pyplot.plot(detailed_times , evolution,  'b', label = 'fit')
+print 'fit to f_Rabi is ', f_Rabi()
+evolution = flop.compute_state_evolution( nbar(), delta, f_Rabi(), detailed_times - offset_time )
+pyplot.plot(detailed_times*10**6 , evolution,  'b', label = 'fit',linewidth=2)
 
-pyplot.plot(times, prob, '--o', label = 'heating {} ms'.format(wait_time))
+pyplot.plot(times*10**6, prob, 'o', label = 'heating {} ms'.format(wait_time),color='red')
 
 pyplot.hold(True)
 pyplot.legend()
 pyplot.title(title)
 pyplot.xlabel('time (us)')
 pyplot.ylabel('D state occupation probability')
-pyplot.text(max(times)*0.70,0.68, 'detuning = {0}'.format(delta))
-pyplot.text(max(times)*0.70,0.73, 'nbar = {:.0f}'.format(nbar()))
-pyplot.text(max(times)*0.70,0.78, 'Rabi Time = {:.1f} us'.format(10**6 * T_Rabi()))
+pyplot.text(max(times)*0.85*10**6,0.78, 'detuning = {0}'.format(delta))
+pyplot.text(max(times)*0.85*10**6,0.83, 'nbar = {:.2f}'.format(nbar()))
+pyplot.text(max(times)*0.85*10**6,0.88, 'Rabi Frequency f = {:.1f} kHz'.format(10**-3 * f_Rabi()/(2.*np.pi)))
 pyplot.ylim([0,1])
 pyplot.legend()
 pyplot.show()
