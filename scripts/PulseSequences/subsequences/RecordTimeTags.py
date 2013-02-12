41806d47b60ae097c0845ead8c3d1250e22787c0
Hlab, lab-197 <haeffnerlab@gmail.com>
2013-02-12 13:39:41 -0800

    day to day changes

--- scripts/PulseSequences/subsequences/RecordTimeTags.py
+++ scripts/PulseSequences/subsequences/RecordTimeTags.py
@@ -2,10 +2,8 @@ from common.okfpgaservers.pulser.pulse_sequences.pulse_sequence import pulse_seq
 
 class record_timetags(pulse_sequence):
     
-    @classmethod
-    def required_parameters(cls):
-        return ['record_timetags_duration']
+    required_parameters = ['record_timetags_duration']
     
     def sequence(self):
-        self.end = self.start + self.p.record_timetags_duration
-        self.ttl_pulses.append(('TimeResolvedCount', self.start, self.p.record_timetags_duration))
+        self.end = self.start + self.record_timetags_duration
+        self.ttl_pulses.append(('TimeResolvedCount', self.start, self.record_timetags_duration))
