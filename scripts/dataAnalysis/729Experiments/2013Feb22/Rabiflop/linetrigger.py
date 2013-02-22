00816d83e0fadaa532d06e6b23eef26770c61546
Firstname <haeffnerlab@gmail.com>
2013-02-22 13:09:26 -0800

    add data analysis plot for sb cooling

---
+++ scripts/dataAnalysis/729Experiments/2013Feb22/Rabiflop/linetrigger.py
@@ -0,0 +1,49 @@
+import labrad
+import numpy
+import matplotlib
+matplotlib.use('Qt4Agg')
+from matplotlib import pyplot
+#get access to servers
+cxn = labrad.connect()
+dv = cxn.data_vault
+figure = pyplot.figure()
+pyplot.title("Carrier at different trigger delay")
+
+dv.cd(['', 'Experiments', '729Experiments', 'Spectrum', '2013Feb14', '1122_48'])
+dv.open(1)
+data = dv.get().asarray
+x = (data[:,0]+15.7)*1000
+pyplot.plot(x, data[:,1],'o-')
+
+dv.cd(['', 'Experiments', '729Experiments', 'Spectrum', '2013Feb14', '1124_04'])
+dv.open(1)
+data = dv.get().asarray
+x = (data[:,0]+15.7)*1000
+pyplot.plot(x, data[:,1],'o-')
+
+dv.cd(['', 'Experiments', '729Experiments', 'Spectrum', '2013Feb14', '1125_09'])
+dv.open(1)
+data = dv.get().asarray
+x = (data[:,0]+15.7)*1000
+pyplot.plot(x, data[:,1],'o-')
+
+dv.cd(['', 'Experiments', '729Experiments', 'Spectrum', '2013Feb14', '1125_47'])
+dv.open(1)
+data = dv.get().asarray
+x = (data[:,0]+15.7)*1000
+pyplot.plot(x, data[:,1],'o-')
+
+dv.cd(['', 'Experiments', '729Experiments', 'Spectrum', '2013Feb14', '1126_28'])
+dv.open(1)
+data = dv.get().asarray
+x = (data[:,0]+15.7)*1000
+pyplot.plot(x, data[:,1],'o-')
+
+#figure.suptitle('QuickMeasurements, Power Monitoring 3')
+#pyplot.xlabel('Time (sec)')
+#pyplot.ylabel('Voltages, mV')
+pyplot.ylabel('Excitation Probability')
+pyplot.xlabel('Frequency (kHz)')
+pyplot.ylim([0,1])
+pyplot.xlim([-12,-8])
+pyplot.show()
