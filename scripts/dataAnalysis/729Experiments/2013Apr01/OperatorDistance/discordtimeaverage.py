6eb78d490bd5d693d2a5a179bdbb0ae688c50bcc
Haeffner Lab <haeffnerlab@gmail.com>
2013-04-02 14:18:53 -0700

    more font size changes

--- scripts/dataAnalysis/729Experiments/2013Apr01/OperatorDistance/discordtimeaverage.py
+++ scripts/dataAnalysis/729Experiments/2013Apr01/OperatorDistance/discordtimeaverage.py
@@ -103,22 +103,27 @@ figure = pyplot.figure()
 
 #pyplot.plot(flop_x_axis*10**6,flop_y_axis, 'ro')
 #pyplot.plot(deph_x_axis*10**6,deph_y_axis, 'bs')
-pyplot.xlabel(r'$\frac{\Omega t}{2\pi}$',fontsize=22)
+fake_f = 100000
+
+timescale = evo.effective_rabi_coupling(nb())*2.0*np.pi
+label = r'$\frac{\Omega_{eff} t}{2}$'
+
+pyplot.xlabel(r'Excitation time '+label,fontsize=44)
 pyplot.ylim((0,1))
 #pyplot.ylabel('Operator Distance')
 #pyplot.legend()
 
-fake_f = 100000
 fake_times = np.linspace(np.array(times).min()/(fake_f),np.array(times).max()/(fake_f),1000)
 discord = evo.discord(fake_times, nbar, fake_f)
 flop = evo.state_evolution(fake_times, nbar, fake_f)
 
-pyplot.plot(np.array(times),2.0*np.array(averages),'ko',label='Time-Averaged Qubit Distance')
-pyplot.plot(fake_times*fake_f,discord,'k-',label='Discord between Qubit and Motion')
-pyplot.plot(fake_times*fake_f,flop,'b-',label='Blue Sideband Rabi Flop, nbar = {:.2f}'.format(nbar))
+pyplot.plot(np.array(times)*timescale,2.0*np.array(averages),'ko',label='Time-Averaged Qubit Distance')
+pyplot.plot(fake_times*fake_f*timescale,discord,'k-',label='Discord between Qubit and Motion')
+pyplot.plot(fake_times*fake_f*timescale,flop,'b-',label='Blue Sideband Rabi Flop, nbar = {:.2f}'.format(nbar))
 
-pyplot.legend(loc=2,prop={'size':20})
-pyplot.title('Time-Averaged Distance to Assess Quantum Discord',fontsize=30)
-pyplot.tick_params(axis='x', labelsize=20)
-pyplot.tick_params(axis='y', labelsize=20)
+pyplot.legend(loc=2,prop={'size':40})
+pyplot.title('Time-Averaged Distance to Assess Quantum Discord',fontsize=60)
+pyplot.tick_params(axis='x', labelsize=40)
+pyplot.tick_params(axis='y', labelsize=40)
+pyplot.xticks([np.pi/4.0,3.0*np.pi/4.0,np.pi/2.0,np.pi,5.0*np.pi/4.0,3.0*np.pi/2.0],[r'$\frac{\pi}{4}$',r'$\frac{3\pi}{4}$',r'$\frac{\pi}{2}$',r'$\pi$',r'$\frac{5\pi}{4}$',r'$\frac{3\pi}{2}$'])
 pyplot.show()
