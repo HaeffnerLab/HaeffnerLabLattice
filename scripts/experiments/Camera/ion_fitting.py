35d87c30bcd52234c3aa9f5137ab3567ee11a3c4
Hlab, lab-197 <haeffnerlab@gmail.com>
2013-05-13 17:43:16 -0700

    organziing files, camera takes reference image

--- scripts/experiments/Camera/ion_fitting.py
+++ scripts/experiments/Camera/ion_fitting.py
@@ -1,46 +1,84 @@
 from __future__ import division
 import numpy as np
 from equilbrium_positions import position_dict
+import lmfit
 
-def ion_gaussian(xx, yy, x_center, y_center, sigma_x, sigma_y, amplitude):
-    '''
-    xx and yy are the provided meshgrid of x and y coordinates
-    generates a 2D gaussian for an ion centered at x_center and y_center
-    '''
-    result = amplitude * np.exp( - (xx - x_center)**2 / (2 * sigma_x**2)) *  np.exp( - (yy - y_center)**2 / (2 * sigma_y**2))
-    return result
+class linear_chain_fitter(object):
 
-def ion_model(params, xx, yy):
-    '''
-    model for fitting ions on the CCD image
-    xx and yy are the provided meshgrid of the coordinate system and zz are the values
-    '''
-    ion_number = int(params['ion_number'].value)
-    background_level = params['background_level'].value
-    amplitude = params['amplitude'].value #all ions are assumsed to have the same amplitude
-    rotation_angle = params['rotation_angle'].value #angle of the ion axis with respect to the xx and yy coordinate system
-    ion_center_x = params['center_x'].value #center of the ions
-    ion_center_y = params['center_y'].value #center of the ions
-    spacing = params['spacing'].value #spacing constant of the ions
-    sigma = params['sigma'].value #width along the axis
+    def ion_gaussian(self, xx, yy, x_center, y_center, sigma_x, sigma_y, amplitude):
+        '''
+        xx and yy are the provided meshgrid of x and y coordinates
+        generates a 2D gaussian for an ion centered at x_center and y_center
+        '''
+        result = amplitude * np.exp( - (xx - x_center)**2 / (2 * sigma_x**2)) *  np.exp( - (yy - y_center)**2 / (2 * sigma_y**2))
+        return result
+
+    def ion_model(self, params, xx, yy):
+        '''
+        model for fitting ions on the CCD image
+        xx and yy are the provided meshgrid of the coordinate system and zz are the values
+        '''
+        ion_number = int(params['ion_number'].value)
+        background_level = params['background_level'].value
+        amplitude = params['amplitude'].value #all ions are assumsed to have the same amplitude
+        rotation_angle = params['rotation_angle'].value #angle of the ion axis with respect to the xx and yy coordinate system
+        ion_center_x = params['center_x'].value #center of the ions
+        ion_center_y = params['center_y'].value #center of the ions
+        spacing = params['spacing'].value #spacing constant of the ions
+        sigma = params['sigma'].value #width along the axis
+        
+        #first we rotate the data by the rotation_angle
+        xx_rotated = xx * np.cos(rotation_angle) - yy  * np.sin(rotation_angle)
+        yy_rotated = xx * np.sin(rotation_angle) + yy  * np.cos(rotation_angle)
+        
+        fit = np.ones_like(xx) * background_level
+        for i in range(ion_number):
+            x_center = ion_center_x + spacing * position_dict[ion_number][i]
+            y_center = ion_center_y
+            fit += self.ion_gaussian(xx_rotated, yy_rotated, x_center, y_center, sigma, sigma, amplitude)
+        return fit
+
+    def ion_chain_fit(self, params , xx, yy,  data):
+        model = self.ion_model(params, xx, yy)
+        return (model - data).ravel()
+
+    def guess_parameters_and_fit(self, xx, yy, data, ion_number):
+        params = lmfit.Parameters()
+        params.add('ion_number', value = ion_number, vary = False)
+        background_guess = np.average(data) #assumes that the data is mostly background apart from few peaks
+        background_std = np.std(data)
+        amplitude_guess = background_guess + 3 * background_std
+        peaks_y, peaks_x = np.where(data > amplitude_guess)
+        center_y_guess= peaks_y.mean()
+        center_x_guess = peaks_x.mean()
+        #assumes that chain is mostly oriented along the x
+        sigma_guess = np.std(peaks_y)
+        spacing_guess = 5 * sigma_guess #assumes we can resolve the ions well
+        params.add('background_level', value = background_guess, min = 0.0)
+        params.add('amplitude', value = amplitude_guess, min = 0.0)
+        params.add('rotation_angle', value = 0, min = 0.0, max = np.pi)
+        params.add('center_x', value = center_x_guess, min = 0, max = 657)
+        params.add('center_y', value = center_y_guess, min = 0, max = 495)
+        params.add('spacing', value = spacing_guess, min = 0.0, max = 495)
+        params.add('sigma', value = sigma_guess, min = 0.0, max = 400)
+        result = lmfit.minimize(self.ion_chain_fit, params, args = (xx, yy, data))
+        return result, params
     
-    #first we rotate the data by the rotation_angle
-    xx_rotated = xx * np.cos(rotation_angle) - yy  * np.sin(rotation_angle)
-    yy_rotated = xx * np.sin(rotation_angle) + yy  * np.cos(rotation_angle)
+    def report(self, params):
+        lmfit.report_errors(params)
     
-    fit = np.ones_like(xx) * background_level
-    for i in range(ion_number):
-        x_center = ion_center_x + spacing * position_dict[ion_number][i]
-        y_center = ion_center_y
-        fit += ion_gaussian(xx_rotated, yy_rotated, x_center, y_center, sigma, sigma, amplitude)
-    return fit
-
-def linear_ion_chain_fit(params , xx, yy,  data):
-    model = ion_model(params, xx, yy)
-    return (model - data).ravel()
+    def graph(self, x_axis, y_axis, image, result):
+        #plot the sample data
+        import matplotlib
+        matplotlib.use('Qt4Agg')
+        from matplotlib import pyplot
+        pyplot.contourf(x_axis, y_axis, image, alpha = 0.1)
+        result = image + np.reshape(result.residual, (y_axis.size, x_axis.size))
+        #plot the fit
+        pyplot.contour(x_axis, y_axis, result, 5)
+        pyplot.show()
 
 if __name__ == '__main__':
-    import lmfit
     #generate sample data
     x_axis = np.linspace(0, 100, 101)
     y_axis = np.linspace(0, 150, 151)
@@ -54,34 +92,16 @@ if __name__ == '__main__':
     sample_params.add('center_y', value = 75.0)
     sample_params.add('spacing', value = 15.0)
     sample_params.add('sigma', value = 3.0)
-    sample_data = ion_model(sample_params, xx, yy)
+    fitter = linear_chain_fitter()
+    sample_data = fitter.ion_model(sample_params, xx, yy)
     #plot the sample data
     import matplotlib
     matplotlib.use('Qt4Agg')
     from matplotlib import pyplot
     pyplot.contourf(x_axis, y_axis, sample_data, alpha = 0.1)
     #perform the fit with some some guessed starting parameters
-    params = lmfit.Parameters()
-    params.add('ion_number', value = 5)
-    background_guess = np.average(sample_data) #assumes that the data is mostly background apart from few peaks
-    background_std = np.std(sample_data)
-    amplitude_guess = background_guess + 3 * background_std
-    peaks_y, peaks_x = np.where(sample_data > amplitude_guess)
-    center_y_guess= peaks_y.mean()
-    center_x_guess = peaks_x.mean()
-    #assumes that chain is mostly oriented along the x
-    sigma_guess = np.std(peaks_y)
-    spacing_guess = 5 * sigma_guess #assumes we can resolve the ions well
-    
-    params.add('background_level', value = background_guess, min = 0.0)
-    params.add('amplitude', value = amplitude_guess, min = 0.0)
-    params.add('rotation_angle', value = 0)
-    params.add('center_x', value = center_x_guess, min = 0)
-    params.add('center_y', value = center_y_guess, min = 0)
-    params.add('spacing', value = spacing_guess, min = 0.0)
-    params.add('sigma', value = sigma_guess, min = 0.0)
-    result = lmfit.minimize(linear_ion_chain_fit, params, args = (xx, yy, sample_data))
-    lmfit.report_errors(params)
+    result, params = fitter.guess_parameters_and_fit(xx, yy, sample_data, ion_number = 5)
+    fitter.report(params)
     result = sample_data + np.reshape(result.residual, (y_axis.size, x_axis.size))
     #plot the fit
     pyplot.contour(x_axis, y_axis, result, 5)
---
+++ scripts/experiments/Camera/reference_image.py
@@ -0,0 +1,79 @@
+from common.abstractdevices.script_scanner.scan_methods import experiment
+import numpy as np
+from ion_fitting import linear_chain_fitter
+
+class reference_camera_image(experiment):
+    
+    name = 'Reference Camera Image'
+    required_parameters = [
+                           ('IonsOnCamera','ion_number'),
+                           ('IonsOnCamera','reference_exposure_time'),
+                           ('IonsOnCamera','vertical_min'),
+                           ('IonsOnCamera','vertical_max'),
+                           ('IonsOnCamera','horizontal_min'),
+                           ('IonsOnCamera','horizontal_max'),
+                           ]
+    
+
+    def initialize(self, cxn, context, ident):
+        self.fitter = linear_chain_fitter()
+        self.ident = ident
+        self.camera = cxn.andor_server
+        self.pv = cxn.parametervault
+        p = self.parameters.IonsOnCamera
+        image_region = [
+                             1,#bin_x
+                             1,#bin_y
+                             int(p.horizontal_min),
+                             int(p.horizontal_max),
+                             int(p.vertical_min),
+                             int(p.vertical_max),
+                             ]
+        self.camera.abort_acquisition()
+        self.initial_exposure = self.camera.get_exposure_time()
+        self.camera.set_exposure_time(p.reference_exposure_time)
+        self.initial_region = self.camera.get_image_region()
+        self.camera.set_image_region(*image_region)
+        self.camera.set_acquisition_mode('Single Scan')
+
+    def run(self, cxn, context):
+        p = self.parameters.IonsOnCamera
+        self.camera.start_acquisition()
+        self.camera.wait_for_acquisition()
+        image = self.camera.get_acquired_data().asarray
+        pixels_x = p.horizontal_max - p.horizontal_min + 1
+        pixels_y = p.vertical_max - p.vertical_min + 1
+        image = np.reshape(image, (pixels_y, pixels_x))
+        self.fit_and_plot(image)
+        
+    def fit_and_plot(self, image):
+        p = self.parameters.IonsOnCamera
+        x_axis = np.arange(p.horizontal_min, p.horizontal_max + 1)
+        y_axis = np.arange(p.vertical_min, p.vertical_max + 1)
+        xx, yy = np.meshgrid(x_axis, y_axis)
+        result, params = self.fitter.guess_parameters_and_fit(xx, yy, image, p.ion_number)
+        self.fitter.report(params)
+        from multiprocessing import Process
+        p = Process(target = self.fitter.graph, args = (x_axis, y_axis, image, result))
+        #self.fitter.graph(x_axis, y_axis, image, result)
+        p.start()
+        self.pv.set_parameter('IonsOnCamera','fit_background_level', params['background_level'].value)
+        self.pv.set_parameter('IonsOnCamera','fit_amplitude', params['amplitude'].value)
+        self.pv.set_parameter('IonsOnCamera','fit_rotation_angle', params['rotation_angle'].value)
+        self.pv.set_parameter('IonsOnCamera','fit_center_horizontal', params['center_x'].value)
+        self.pv.set_parameter('IonsOnCamera','fit_center_vertical', params['center_y'].value)
+        self.pv.set_parameter('IonsOnCamera','fit_spacing', params['spacing'].value)
+        self.pv.set_parameter('IonsOnCamera','fit_sigma', params['sigma'].value)
+
+    def finalize(self, cxn, context):
+        self.camera.set_exposure_time(self.initial_exposure)
+        self.camera.set_image_region(self.initial_region)
+        self.camera.start_live_display()
+
+if __name__ == '__main__':
+    import labrad
+    cxn = labrad.connect()
+    scanner = cxn.scriptscanner
+    exprt = reference_camera_image(cxn = cxn)
+    ident = scanner.register_external_launch(exprt.name)
+    exprt.execute(ident)
