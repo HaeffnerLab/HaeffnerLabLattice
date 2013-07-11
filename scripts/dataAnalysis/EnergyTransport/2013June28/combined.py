b09fb302e94ee4cb5cdf83f9e8bd795dcc570da3
Hlab, lab-197 <haeffnerlab@gmail.com>
2013-07-11 15:18:44 -0700

    separate plots for heating

--- scripts/dataAnalysis/EnergyTransport/2013June28/combined.py
+++ scripts/dataAnalysis/EnergyTransport/2013June28/combined.py
@@ -1,24 +1,24 @@
 import labrad
 from matplotlib import pyplot
 
-info = [('2013Jun28', '1924_31'),
+info = [('2013Jun28', '1924_31', 'b'),
 #         ('2013Jun28', '1925_51'),
-        ('2013Jun28', '1926_42'),
-        ('2013Jun28', '1927_29'),
-        ('2013Jun28', '1928_29'),
+        ('2013Jun28', '1926_42', 'g'),
+        ('2013Jun28', '1927_29', 'r'),
+        ('2013Jun28', '1928_29', 'c'),
         
         ]
 
 cxn = labrad.connect()
 dv = cxn.data_vault
 
-for date, datasetName in info:
+for date, datasetName, color in info:
     dv.cd( ['','Experiments','Blue Heat RabiFlopping',date,datasetName])
     dv.open(1)
     data = dv.get().asarray
     times, probs = data.transpose()
     heating_us = dv.get_parameter('Heating.blue_heating_duration')['us']
-    pyplot.plot(times, probs, 'o-', label = r'Heating, {0} $\mu s$'.format(heating_us))
+    pyplot.plot(times, probs, 'o-', label = r'Heating, {0} $\mu s$'.format(heating_us), color = color)
 
 pyplot.title('Pulsed Heating', fontsize = 32)
 pyplot.xlabel(u'Excitation time $\mu s$', fontsize = 32)
