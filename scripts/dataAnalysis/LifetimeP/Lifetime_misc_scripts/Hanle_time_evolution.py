208e9d960d96d1aaa8fdc85226aa815d10f18fd5
Firstname <haeffnerlab@gmail.com>
2013-12-09 14:32:13 -0800

    add N5747A controller

--- scripts/dataAnalysis/LifetimeP/Lifetime_misc_scripts/Hanle_time_evolution.py
+++ scripts/dataAnalysis/LifetimeP/Lifetime_misc_scripts/Hanle_time_evolution.py
@@ -90,34 +90,56 @@ class Hanle:
 		return rho_dot
 
 Hanle = Hanle()
-Omega = 0.1
-gamma = 1.0
-delta = 0.1 ## this is the Zeeman splitting
+Omega = 1.0
+gamma = 0.0
+delta_array = np.linspace(-2,2,20) ## this is the Zeeman splitting
 theta = np.pi/2.0
-Delta = 0.0 ## this is the laser detuning
+Delta0 = 0.0 ## this is the laser detuning
 p = 0.93565
-delta_t = 1.0
+delta_t = 0.0
+beta_array = np.array([0,20.0])
 
-rho0 = np.zeros(16)
-rho0[0] = 1.0
 
-excitation = []
-ground_state = []
 
-for k in range(1,1000):
-	k1 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0)
-	k2 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0+k1/2.0)
-	k3 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0+k2/2.0)
-	k4 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0+k3)
-	rho1 = rho0 + (k1+2*k2+2*k3+k4)/6.0
-	rho0 = rho1
-	ground_state.append(rho1[0])
-	excitation.append(rho1[10]+rho1[15])
+for beta in beta_array:
+	hanle_signal = []
+	pyplot.figure(0)
+	for delta in delta_array:
+		rho0 = np.zeros(16)
+		rho0[0] = 0.5
+		rho0[5] = 0.5
+		excitation = []
+		ground_state = []
+		time_array = []
+		absolute_time = 0.0
+		area = 0.0
+		for k in range(1,100000):
+			delta_t = 0.05
+			Delta = beta*np.sin(2*np.pi*1.57*absolute_time)+Delta0
+			k1 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0)
+			k2 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0+k1/2.0)
+			k3 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0+k2/2.0)
+			k4 = delta_t*Hanle.get_rho_dot(Omega, Delta, theta, gamma, delta, p, rho0+k3)
+			rho1 = rho0 + (k1+2*k2+2*k3+k4)/6.0
+			rho0 = rho1
+			absolute_time = absolute_time + delta_t
+			time_array.append(absolute_time)
+			hanle_signal_excitation = np.real(rho1[10]+rho1[15])+np.imag(rho1[11]-rho1[14])
+			#hanle_signal_excitation = np.imag(rho1[11]-rho1[14])
+			area = area + hanle_signal_excitation*delta_t
+			#ground_state.append(rho1[0])
+			excitation.append(hanle_signal_excitation)
+		print delta
+		pyplot.plot(time_array,np.real(excitation))
+		hanle_signal.append(area) 
+	pyplot.figure(1)
+	pyplot.plot(2.0*delta_array/3.0,hanle_signal)
+#print excitation
+#print area 
+#pyplot.plot(np.real(excitation))
+# pyplot.plot(np.real(ground_state))
 
-print excitation
-
-pyplot.plot(np.real(excitation))
-#pyplot.plot(np.real(ground_state))
+print hanle_signal
 pyplot.show()	
 
 	
