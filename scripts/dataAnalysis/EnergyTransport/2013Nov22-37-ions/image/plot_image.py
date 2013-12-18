4b852c32a2b08b0cfb171497ee60bfea3dd6dc09
Firstname <haeffnerlab@gmail.com>
2013-12-18 15:06:43 -0800

    additional data anlysis

--- scripts/dataAnalysis/EnergyTransport/2013Nov22-37-ions/image/plot_image.py
+++ scripts/dataAnalysis/EnergyTransport/2013Nov22-37-ions/image/plot_image.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot
 
 image = np.loadtxt('37_ion_local.asc',dtype = int,delimiter =',', usecols = range(1,497))
 image = image.transpose()
-image = image[200:275,225:430]
+image = image[200:275,210:430]
 print image
 fig,ax = pyplot.subplots()
 imgplot = ax.imshow(image, cmap=pyplot.cm.gray, interpolation='nearest')
