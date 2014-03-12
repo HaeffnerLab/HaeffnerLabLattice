165f1dcb721e3134ffd9bb46152c876312839640
Hlab, lab-197 <haeffnerlab@gmail.com>
2014-03-12 14:42:28 -0700

    new ramsey 2 ion simulaneously drive two ions

--- scripts/PulseSequences/subsequences/Ramsey_2ions.py
+++ scripts/PulseSequences/subsequences/Ramsey_2ions.py
@@ -30,12 +30,15 @@ class ramsey_2ions_excitation(pulse_sequence):
     def sequence(self):
         #this hack will be not needed with the new dds parsing methods
         p = self.parameters.Ramsey_2ions
-        frequency_advance_duration = WithUnit(6, 'us')
-        gap = WithUnit(2, 'us')
+        frequency_advance_duration = WithUnit(8.0, 'us')
+        gap = WithUnit(1.0, 'us')
         ampl_off = WithUnit(-63.0, 'dBm')
+        
+        #detuning = WithUnit(0.0,'kHz')
         
         #self.end = self.start + frequency_advance_duration + p.rabi_excitation_duration
         self.end = self.start
+        #print self.end
         ###set all frequencies but keep amplitude low first###
         self.addDDS('729', self.end, frequency_advance_duration, p.ion1_excitation_frequency1, ampl_off)
         self.addDDS('729_1', self.end, frequency_advance_duration, p.ion1_excitation_frequency2, ampl_off)        
@@ -43,60 +46,30 @@ class ramsey_2ions_excitation(pulse_sequence):
         self.addDDS('729_aux_1', self.end, frequency_advance_duration, p.ion2_excitation_frequency2, ampl_off)
         self.end = self.end + frequency_advance_duration
         
-        ###first rabi excitation on ion1###
+        ###pi/2 pulses###
         self.addDDS('729', self.end, p.ion1_excitation_duration1, p.ion1_excitation_frequency1, p.ion1_excitation_amplitude1)
-        print 'left ion pulse 1:', p.ion1_excitation_duration1, p.ion1_excitation_frequency1, p.ion1_excitation_amplitude1
-        
-        
-        self.end = self.end + p.ion1_excitation_duration1 + gap
-        
-        ###second rabi excitation on ion1###                
-        self.addDDS('729_1', self.end, p.ion1_excitation_duration2, p.ion1_excitation_frequency2, p.ion1_excitation_amplitude2)
-        print 'left ion pulse 2:', p.ion1_excitation_duration2, p.ion1_excitation_frequency2, p.ion1_excitation_amplitude2
-        self.end = self.end + p.ion1_excitation_duration2 + gap       
-        
-        ###first rabi excitation on ion2###
         self.addDDS('729_aux', self.end, p.ion2_excitation_duration1, p.ion2_excitation_frequency1, p.ion2_excitation_amplitude1)
-        print 'right ion pulse 1:',p.ion2_excitation_duration1, p.ion2_excitation_frequency1, p.ion2_excitation_amplitude1
-        self.end = self.end + p.ion2_excitation_duration1 + gap   
+        #print 'left ion pulse 1:', p.ion1_excitation_duration1, p.ion1_excitation_frequency1, p.ion1_excitation_amplitude1
+        self.end = self.end + max(p.ion1_excitation_duration1,p.ion2_excitation_duration1) + gap
         
-        ###second rabi excitation on ion2###                
-        self.addDDS('729_aux_1', self.end, p.ion2_excitation_duration2, p.ion2_excitation_frequency2, p.ion2_excitation_amplitude2)
-        print 'right ion pulse 2:',p.ion2_excitation_duration2, p.ion2_excitation_frequency2, p.ion2_excitation_amplitude2
-        self.end = self.end + p.ion2_excitation_duration2 + gap
-        stop_pulses = self.end
-        ###add ramsey time which we do nothing###
-        #self.end = self.end+p.ramsey_time
+        ###pi pulses###
         
-        doppler_cooling_duration = max(p.ramsey_time - self.parameters.DopplerCooling.doppler_cooling_repump_additional,WithUnit(0, 'us'))
-        
-        replace = TreeDict.fromdict({
-                                     'DopplerCooling.doppler_cooling_duration':doppler_cooling_duration
-                                     #'DopplerCooling.doppler_cooling_amplitude_866':WithUnit(-5.0,'dBm')
-                                     })
-        if self.parameters.Ramsey2ions_ScanGapParity.sympathetic_cooling_enable:
-            self.addSequence(doppler_cooling, replace)
-            #print 'with cooling', stop_pulses, self.end
-        else:
-            self.end = self.end+p.ramsey_time
-            #print 'without cooling', stop_pulses, self.end
-        
-        #self.end = self.end + gap
-        ### add doppler cooling
-        #self.addSequence(doppler_cooling)
-        
-        ###undoing stuff###
-        ###second rabi excitation on ion2###                
-        self.addDDS('729_aux_1', self.end, p.ion2_excitation_duration2, p.ion2_excitation_frequency2, p.ion2_excitation_amplitude2)
-        self.end = self.end + p.ion2_excitation_duration2 + gap
-        ###first rabi excitation on ion2###
-        self.addDDS('729_aux', self.end, p.ion2_excitation_duration1, p.ion2_excitation_frequency1, p.ion2_excitation_amplitude1,p.ion2_excitation_phase1)
-        self.end = self.end + p.ion2_excitation_duration1 + gap
-        print p.ion2_excitation_phase1
-        ###second rabi excitation on ion1###                
         self.addDDS('729_1', self.end, p.ion1_excitation_duration2, p.ion1_excitation_frequency2, p.ion1_excitation_amplitude2)
-        self.end = self.end + p.ion1_excitation_duration2 + gap    
-        ###first rabi excitation on ion1###
-        self.addDDS('729', self.end, p.ion1_excitation_duration1, p.ion1_excitation_frequency1, p.ion1_excitation_amplitude1)
-        self.end = self.end + p.ion1_excitation_duration1 + gap
-
+        self.addDDS('729_aux_1', self.end, p.ion2_excitation_duration2, p.ion2_excitation_frequency2, p.ion2_excitation_amplitude2)
+        #print 'left ion pulse 2:', p.ion1_excitation_duration2, p.ion1_excitation_frequency2, p.ion1_excitation_amplitude2
+        self.end = self.end + max(p.ion1_excitation_duration2,p.ion2_excitation_duration2) + gap   
+        
+        ### ramsey time ###
+        
+        self.end = self.end+p.ramsey_time
+        
+        self.addDDS('729_1', self.end, p.ion1_excitation_duration2, p.ion1_excitation_frequency2, p.ion1_excitation_amplitude2,WithUnit(180,'deg'))
+        self.addDDS('729_aux_1', self.end, p.ion2_excitation_duration2, p.ion2_excitation_frequency2, p.ion2_excitation_amplitude2,WithUnit(180,'deg'))
+        #print 'left ion pulse 2:', p.ion1_excitation_duration2, p.ion1_excitation_frequency2, p.ion1_excitation_amplitude2
+        self.end = self.end + max(p.ion1_excitation_duration2,p.ion2_excitation_duration2) + gap  
+        
+        ###pi/2 pulses###
+        self.addDDS('729', self.end, p.ion1_excitation_duration1, p.ion1_excitation_frequency1, p.ion1_excitation_amplitude1,WithUnit(180,'deg'))
+        self.addDDS('729_aux', self.end, p.ion2_excitation_duration1, p.ion2_excitation_frequency1, p.ion2_excitation_amplitude1,p.ion2_excitation_phase1)
+        #print 'left ion pulse 1:', p.ion1_excitation_duration1, p.ion1_excitation_frequency1, p.ion1_excitation_amplitude1
+        self.end = self.end + max(p.ion1_excitation_duration1,p.ion2_excitation_duration1) + gap
