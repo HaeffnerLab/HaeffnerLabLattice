a111fafb12381c89eca80dbc859d80478d3edff7
Thaned Pruttivarasin <thanedp@thaneds-air.gateway.2wire.net>
2014-03-12 21:32:47 -0700

    revised allan deviation

--- scripts/dataAnalysis/LLI/2014_03_12_phase_allan.py
+++ scripts/dataAnalysis/LLI/2014_03_12_phase_allan.py
@@ -22,32 +22,47 @@ data = dv.get().asarray
 time = data[:,0]
 time = time-time[0]
 phase = data[:,1]
+
+time = time[75:299]
+time = time-time[0]
+phase = phase[75:299]
+
 ramsey_time = 0.013
 
 phase = phase/360.0/ramsey_time ## convert phase to frequency sensitivity
 
 interval = time[1:]-time[0:-1]
 
-start_bin_size = max(interval)+1
+start_bin_size = max(interval)+1 # choose bin size to have at least one data point
 
 ##### Calculate allan deviation ####
 bin_array = []
-allan_deviation = []
+true_variance = []
+avar = []
 allan_error_bar = []
 #for bin_size in np.linspace(start_bin_size,max(time)/2.0,int(max(time)/2.0/start_bin_size)):
-for bin_size in np.linspace(start_bin_size,max(time)/2.0,100):
+for bin_size in np.linspace(0.0,max(time)/2.0,80):
 #print range(0,int(np.floor(max(time)/bin_size)))
+    if bin_size<start_bin_size:
+        continue
     mean_phase_data = []
+    #print int(np.floor(max(time)/bin_size))
     for i in range(0,int(np.floor(max(time)/bin_size))):
         where = np.where((bin_size*i<time)&(time<bin_size*(i+1)))
         mean_phase = np.average(phase[where])
         mean_phase_data.append(mean_phase)
     
     bin_array.append(bin_size) 
-    allan_deviation.append(np.std(mean_phase_data))
-    #print i
-    #allan_error_bar.append(np.std(mean_phase_data)*np.sqrt(2/(i)))
+    true_variance.append(np.std(mean_phase_data))
+    mean_phase_diff = np.array(mean_phase_data[1:])-np.array(mean_phase_data[0:-1])
+    phase_diff = np.sqrt(np.average(mean_phase_diff**2)/2)
+    avar.append(phase_diff)
+    allan_error_bar.append(phase_diff*0.5*np.sqrt(1/(i)))
 
-pyplot.plot(bin_array,allan_deviation,'o')
-#pyplot.errorbar(bin_array,allan_deviation,allan_error_bar)
+pyplot.plot(bin_array,true_variance,'o-')
+pyplot.plot(bin_array,avar,'o-')
+pyplot.errorbar(bin_array,avar,allan_error_bar)
+
+pyplot.xscale('log')
+pyplot.yscale('log')
 pyplot.show()
