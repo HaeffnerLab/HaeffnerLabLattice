2fc1390490b83e563eb5ce55b3b55540e2981a66
Hlab, lab-197 <haeffnerlab@gmail.com>
2014-05-09 14:47:05 -0700

    some data analysis related to LLI measurement

--- scripts/dataAnalysis/LLI/Misc/Cosine_fitter2.py
+++ scripts/dataAnalysis/LLI/Misc/Cosine_fitter2.py
@@ -23,40 +23,24 @@ cxn = labrad.connect()
 dv = cxn.data_vault
 
 
-dv.cd(['','Experiments','Parity_LLI_scan_gap','2014May04','1214_49'])
+dv.cd(['','Experiments','Parity_LLI_scan_gap','2014May04','1123_52'])
 dv.open(2)
 data = dv.get().asarray
-dv.cd(['','Experiments','Parity_LLI_scan_gap','2014May04','1254_17'])
-dv.open(2)
-data2 = dv.get().asarray
-x = data[:,0]/1000
-x2 = data2[:,0]/1000
-y = np.multiply(data[:,1]*0.45/0.225,np.exp(-x/180))
-y2 = np.multiply(data2[:,1]*0.45/0.225,np.exp(-x2/180))
-x = x[3:]
-y = y[3:]
-
-x = np.concatenate((x,x2),axis=0)
-y = np.concatenate((y,y2),axis=0)
-yerr = 1/np.sqrt((1-y**2)/800)
-
-### binner
-for i in range(1,np.size(x)-1):
-    print i
-    y[i] = (y[i-1]+y[i]+y[i+1])/3.0
-    i = i+1
+x = data[:,0]/1000000
+y = data[:,1]
+yerr = 1/np.sqrt(800)
 
 figure = pyplot.figure(1)
 figure.clf()
 
-
+pyplot.plot(x,y,'o')
 
 ##fitter
 params = lmfit.Parameters()
 
 params.add('A', value = 0.4)
-params.add('tau', value = 180)
-params.add('freq', value = 0.163)
+params.add('tau', value = 1000000)
+params.add('freq', value = 163)
 params.add('phase', value = 0.0)
 
 result = lmfit.minimize(cosine_fit, params, args = (x, y, yerr))
@@ -71,9 +55,7 @@ lmfit.report_errors(params)
 x_plot = np.linspace(x.min(),x.max(),1000)
 
 
-pyplot.plot(x,y,'o',color='#00246B',zorder=3)
-pyplot.plot(x_plot,cosine_model(params,x_plot),'-',linewidth=1.5,color="#A0A8B8",zorder=2)
-pyplot.xlim([-1,105])
-pyplot.ylim([-0.6,0.6])
+pyplot.plot(x,y,'o')
+pyplot.plot(x_plot,cosine_model(params,x_plot))
 
 pyplot.show()
