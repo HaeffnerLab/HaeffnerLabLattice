4160e258ef0321fd4ca93da34dde95b312799a55
Dylan J MacDonald Gorman <dylan@nfs.haeffnerlab.edu>
2015-07-10 13:46:36 -0700

    Adding in experiments for 1 ion szx gate

--- scripts/experiments/Gates/excitations.py
+++ scripts/experiments/Gates/excitations.py
@@ -4,3 +4,8 @@ class molmer_sorensen_gate(base_excitation):
     from lattice.scripts.PulseSequences.molmer_sorensen_gate import ms_gate
     name = 'MolmerSorensenGate'
     pulse_sequence = ms_gate
+
+class szx_gate(base_excitation):
+    from lattice.scripts.PulseSequences.szx_1ion import szx_1ion
+    name = 'SZXGate'
+    pulse_sequence = szx_1ion
