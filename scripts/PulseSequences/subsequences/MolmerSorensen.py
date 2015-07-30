f0784dcfbbafd6254ede1bd24d96f86a2e7c9c7e
Dylan J MacDonald Gorman <dylan@nfs.haeffnerlab.edu>
2015-07-29 18:49:31 -0700

    first attempt to add in a local stark shift

--- scripts/PulseSequences/subsequences/MolmerSorensen.py
+++ scripts/PulseSequences/subsequences/MolmerSorensen.py
@@ -13,12 +13,17 @@ class molmer_sorensen(pulse_sequence):
                           ('MolmerSorensen','analysis_amplitude'),
                           ('MolmerSorensen','analysis_duration'),
                           ('MolmerSorensen','shape_profile'),
+
+                          ('LocalStarkShift', 'enable'),
+                          ('LocalStarkShift','amplitude'),
+                          ('LocalStarkShift', 'detuning'), # detuning from the carrier transition
                           ]
 
     def sequence(self):
         #this hack will be not needed with the new dds parsing methods
         slope_dict = {0:0.0, 2:2.0, 4:5.0, 6:30.0}
         p = self.parameters.MolmerSorensen
+        pl = self.parameters.LocalStarkShift
         frequency_advance_duration = WithUnit(6, 'us')
         try:
             slope_duration = WithUnit(int(slope_dict[p.shape_profile]),'us')
@@ -30,6 +35,11 @@ class molmer_sorensen(pulse_sequence):
         self.addDDS('729', self.start, frequency_advance_duration, p.frequency, ampl_off)
         self.addDDS('729', self.start + frequency_advance_duration, p.duration, p.frequency, p.amplitude, p.phase, profile=int(p.shape_profile))
         self.addTTL('bichromatic_1', self.start, p.duration + 2*frequency_advance_duration + slope_duration)
+        
+        if pl.enable: # add a stark shift on the localized beam
+            f = p.frequency + pl.detuning
+            self.addDDS('729DP_1', self.start, frequency_advance_duration, f, ampl_off)
+            self.addDDS('729DP_1', self.start + frequency_advance_duration, p.duration, f, pl.amplitude, profile=int(p.shape_profile))
         
         if p.analysis_pulse_enable:
             analysis_start = self.end
