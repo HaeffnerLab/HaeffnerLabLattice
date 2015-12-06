9cd5a8ad9739a1566bd7b6a17628e909fa53ffd0
Dylan Gorman <dylangorman@Dylans-MacBook-Pro-2.local>
2015-12-06 11:21:06 -0800

    moved fitting classes to one file

--- scripts/experiments/CalibrationScans/fitters.py
+++ scripts/experiments/CalibrationScans/fitters.py
@@ -23,5 +23,22 @@ class pi_time_fitter():
         t_pi, c = curve_fit(model, t, exci, p0 = t0)
         return t_pi[0]
 
-        
-
+class peak_fitter():
+    
+    def guess(self, f, p):
+        '''
+        just take the point at the peak value
+        '''
+        max_index = np.where(p == p.max())[0][0]
+        fmax = f[max_index]
+        if p.max() <= 0.2:
+            raise Exception("Peak not found")
+        else:
+            # center, amplitude, width guesses
+            return np.array([ fmax,  p.max(), 6e-3 ])
+    
+    def fit(self, f, p):
+        model = lambda x, c0, a, w: a*np.exp(-(x - c0)**2/w**2)
+        guess = self.guess(f, p)
+        popt, copt = curve_fit(model, f, p, p0=guess)
+        return popt[0] # just the fitted center value
