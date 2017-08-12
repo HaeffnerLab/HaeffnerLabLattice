45e5ef315a02fc528d4aa3a8d583609b2c702e3f
EliMegidish <eli.megidish@gmail.com>
2017-08-11 19:03:19 -0700

    Spectrum manual+relative and Ramsey updated

--- PulseSequences2/Spectrum.py
+++ PulseSequences2/Spectrum.py
@@ -1,22 +1,26 @@
-from common.devel.bum.sequences import pulse_sequence
-#from pulse_sequence import pulse_sequence
+#!scriptscanner
+
+from common.devel.bum.sequences.pulse_sequence import pulse_sequence
 from labrad.units import WithUnit as U
 from treedict import TreeDict
 
-class ManualSpectrum(pulse_sequence):
+class Spectrum(pulse_sequence):
     
     name = 'Spectrum729'
                             #(self, scan_param, minim, maxim, steps, unit)
     scannable_params = {
-        'Spectrum.manual_freq':  (-20,20, 2000, 'MHz')
+        #'Spectrum.carrier_detuning':  [(-50, 50, 100, 'kHz'), 'window']
+        'Spectrum.carrier_detuning' : (-50, 50, 100, 'kHz'),
+        'Spectrum.sideband_detuning' :(-50, 50, 100, 'kHz')
               }
 
     show_params= ['Excitation_729.channel_729',
                   'Excitation_729.bichro',
-                  'Spectrum.manual_duration',
-                  'Spectrum.manual_amplitude',
-                     ]
-    
+                  'Spectrum.carrirer_duration',
+                  'Spectrum.carrirer_amplitude',
+                  'Spectrum.line_selection',   
+                  ]
+   
     def run_initial(self):
         pass
 
@@ -28,15 +32,19 @@ class ManualSpectrum(pulse_sequence):
         from subsequences.TurnOffAll import TurnOffAll
            
         ## calculate the scan params
-        freq_729 = self.parameters.Spectrum.manual_freq           
-        amp=self.parameters.Spectrum.manual_amplitude
-        duration=self.parameters.Spectrum.manual_duration
+        spc = self.parameters.Spectrum   
         
-        print "Manual spectrum scan"
+        if spc.selection_sideband == "off":         
+            freq_729=self.calc_freq(spc.line_selection)
+        else:
+            freq_729=self.calc_freq(spc.line_selection, spc.selection_sideband ,int(spc.order))
+        
+        freq_729=freq_729 + spc.carrier_detuning + spc.sideband_detuning
+        
+        print "Spectrum scan"
         print "729 freq.{}".format(freq_729)
         
-        
-        
+                
         # building the sequence
         # needs a 10 micro sec for some reason
         self.end = U(10., 'us')
@@ -46,11 +54,3 @@ class ManualSpectrum(pulse_sequence):
                                          'Excitation_729.rabi_excitation_amplitude': amp,
                                          'Excitation_729.rabi_excitation_duration':  duration })
         self.addSequence(StateReadout)
-
-                
-
-#if __name__=='__main__':
-    #pv = TreeDict.fromdict({'DopplerCooling.duration':U(5, 'us')})
-    #ex = Sequence(pv)
-    #psw = pulse_sequence_wrapper('example.xml', pv)
-#    ManualSpectrum.execute_external(('Spectrum.carrirer_detuning', -50, 50,100, 'kHz'))
