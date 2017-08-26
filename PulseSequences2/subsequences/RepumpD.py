db52b380533347d0b63b0480881abb46f59056dc
Hlab, lab-197 <haeffnerlab@gmail.com>
2017-08-25 18:11:52 -0700

    debugging pulse sequences

--- PulseSequences2/subsequences/RepumpD.py
+++ PulseSequences2/subsequences/RepumpD.py
@@ -5,9 +5,11 @@ class RepumpD(pulse_sequence):
   
     def sequence(self):
         p = self.parameters.RepumpD_5_2
+        st = self.parameters.StateReadout 
         print "START- repump D"
         #print self.start
         #print "p.repump_d_duration"
         #print p.repump_d_duration
         self.end = self.start + p.repump_d_duration
         self.addDDS('854DP', self.start, p.repump_d_duration, p.repump_d_frequency_854, p.repump_d_amplitude_854)
+        self.addDDS ('866',self.start, p.repump_d_duration, st.state_readout_frequency_866, st.state_readout_amplitude_866)
